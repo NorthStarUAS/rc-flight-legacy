748c0412681c4d5477f7b634ea3270c723d5fd23
Curtis L. Olson <curtolson@gmail.com>
2006-10-05 19:31:03 +0000

    Initial revision.

---
+++ src/imugps.h
@@ -0,0 +1,14 @@
+//
+// FILE: imugps.h
+// DESCRIPTION: aquire, validate and decode the IMU, GPS, and Servo data
+//              inbound from the uNAV
+//
+
+#ifndef _UNAV_IMUGPS_H
+#define _UNAV_IMUGPS_H
+
+
+void *imugps_acq(void *thread_id);
+
+
+#endif // _UNAV_IMUGPS_H
---
+++ src/misc.h
@@ -0,0 +1,10 @@
+#ifndef _UNAV_MISC_H
+#define _UNAV_MISC_H
+
+
+extern void snap_time_interval(char *threadname, int displaytime, short id);
+extern double get_time_interval(short id);
+extern double get_Time();
+
+
+#endif // _UNAV_MISC_H
