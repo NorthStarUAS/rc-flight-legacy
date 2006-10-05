0b3678afbc80c84978d401d3e407140f9dd2f8ef
Curtis L. Olson <curtolson@gmail.com>
2006-10-05 19:44:57 +0000

    Code cleanup and reorg.
    Former-commit-id: 825100edb9cda2d0f41be506ee29d8ba5e42bc03

---
+++ src/navigation.h
@@ -0,0 +1,13 @@
+//
+// FILE: navigation.h
+// DESCRIPTION: compute the position estimate
+//
+
+#ifndef _UNAV_NAVIGATION_H
+#define _UNAV_NAVIGATION_H
+
+
+void *navigation(void *thread_id);
+
+
+#endif // _UNAV_NAVIGATION_H
