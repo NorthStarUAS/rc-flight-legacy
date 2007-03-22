38795b404d2b435df6a1a97f684632ba84a3f4c9
Curtis L. Olson <curtolson@gmail.com>
2007-03-22 00:46:45 +0000

    Battery read needs to fail 5 consecutive times before it will give up entirely. Avoids a race condition with the daemon that copies over the voltage to a /tmp file.
    Former-commit-id: 08d8eadad961889b5e9bf5e142cf29bb2f50e98d

--- src/health/sgbatmon.cpp
+++ src/health/sgbatmon.cpp
@@ -11,11 +11,11 @@ static FILE *fbat;
 static float start_volts = 0.0;
 static float volt_cutoff = 3.40;
 static double start_time = 0.0;
-static bool batmon_exists;
+static int batmon_failed;
 
 
 bool sgbatmon_init() {
-    batmon_exists = true;
+    batmon_failed = 0;
     return true;
 }
 
@@ -24,23 +24,30 @@ bool sgbatmon_update() {
     char buf[5];
     int result = 0;
 
-    if ( !batmon_exists ) {
+    if ( batmon_failed > 5 ) {
         return false;
     }
 
-    if ( (fbat = fopen("/dev/platx/batmon", "r")) != NULL ) {
+    // NOTE: on the stargate, the actual battery monitor device name
+    // is /dev/platx/batmon, but it is brutally slow to open and read.
+    // So I created a "niced" background script that copies the value
+    // of /dev/platx/batmon to /tmp/batmon every 10 seconds.  Thus the
+    // real time app can simply open a file and read the value quickly
+    // from there.
+
+    if ( (fbat = fopen("/tmp/batmon", "r")) != NULL ) {
         result = fread( buf, 4, 1, fbat );
         buf[4] = 0;
         if ( result == 1 ) {
             float v = atof(buf);
-            if ( v < 0 ) {
+            if ( v <= 0 ) {
                 // nonsense value keep previous reading.
                 v = healthpacket.volts;
             }
             if ( start_volts < 0.1 ) {
                 healthpacket.volts = v;
                 start_volts = v;
-                    start_time = get_Time();
+		start_time = get_Time();
             }
             // printf("buf = %s, raw volts = %.2f\n", buf, v);
             healthpacket.volts_raw = v;
@@ -57,15 +64,17 @@ bool sgbatmon_update() {
                     healthpacket.est_seconds = (uint16_t)est;
                 }
             }
+
+	    batmon_failed = 0;
         } else {
 	    printf("fread() failed\n");
             fclose( fbat );
-            batmon_exists = false;
+            batmon_failed++;
             return false;
         }
         fclose( fbat );
     } else {
-        batmon_exists = false;
+        batmon_failed++;
         if ( display_on ) {
             printf("Cannot open battery monitor device\n");
         }
