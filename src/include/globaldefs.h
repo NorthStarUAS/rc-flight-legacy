5df1c427f8b6350affc14810e2498729ec12ada6
Curtis L. Olson <curtolson@gmail.com>
2007-07-18 21:10:52 +0000

    Add a few more constants and conversion factors.

--- src/include/globaldefs.h
+++ src/include/globaldefs.h
@@ -65,7 +65,11 @@ extern struct nav navpacket;
 extern struct servo servopacket;
 extern struct health healthpacket;
 
-// constants
+// A varienty of constants
+
+/** For divide by zero avoidance, this will be close enough to zero */
+#define SG_EPSILON 0.0000001
+
 #define M_2PI 6.28318530717958647692
 
 #ifndef M_PI
@@ -74,10 +78,44 @@ extern struct health healthpacket;
 #define SG_PI  ((float) M_PI)
 #endif
 
-#define SG_180   180.0f
+#ifdef M_PI_2
+#  define  SGD_PI_2  M_PI_2
+#else
+#  define  SGD_PI_2  1.57079632679489661923
+#endif
 
+#ifndef M_PI
+#define SGD_PI 3.14159265358979323846   /* From M_PI under Linux/X86 */
+#else
+#define SGD_PI M_PI
+#endif
+
+/** 2 * PI */
+#define SGD_2PI      6.28318530717958647692
+
+#define SG_180   180.0f
+#define SGD_180  180.0
 #define SG_DEGREES_TO_RADIANS  (SG_PI/SG_180)
 #define SG_RADIANS_TO_DEGREES  (SG_180/SG_PI)
+#define SGD_DEGREES_TO_RADIANS  (SGD_PI/SGD_180)
+#define SGD_RADIANS_TO_DEGREES  (SGD_180/SGD_PI)
 
+#define SG_FEET_TO_METER    0.3048
+#define SG_METER_TO_FEET    3.28083989501312335958
+
+/** Nautical Miles to Meters */
+#define SG_NM_TO_METER      1852.0000
+
+/** Meters to Nautical Miles.  1 nm = 6076.11549 feet */
+#define SG_METER_TO_NM      0.0005399568034557235
+
+/** Statute Miles to Meters. */
+#define SG_SM_TO_METER      1609.3412196
+
+/** Radians to Nautical Miles.  1 nm = 1/60 of a degree */
+#define SG_NM_TO_RAD        0.00029088820866572159
+
+/** Nautical Miles to Radians */
+#define SG_RAD_TO_NM        3437.7467707849392526
 
 #endif // _UGEAR_GLOBALDEFS_H
