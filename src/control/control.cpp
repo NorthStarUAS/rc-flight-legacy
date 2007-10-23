f1c98a1f7669f33df1d7b7535b7c210657bb3c79
Curtis L. Olson <curtolson@gmail.com>
2007-10-23 20:45:49 +0000

    Properly time stamp out going servo control structure. Remove old xbow control code that was already ifdef'd out.

--- src/control/control.cpp
+++ src/control/control.cpp
@@ -18,6 +18,7 @@
 #include "include/globaldefs.h"
 #include "navigation/mnav.h"
 #include "props/props.hxx"
+#include "util/timing.h"
 
 #include "util.h"
 #include "xmlauto.hxx"
@@ -116,14 +117,14 @@ void control_update(short flight_mode)
 
     /* printf("%.2f %.2f\n", aileron_out_node->getFloatValue(),
               elevator_out_node->getFloatValue()); */
-    static SGPropertyNode *nav_vert_speed_fps
+    static SGPropertyNode *vert_speed_fps
         = fgGetNode("/velocities/vertical-speed-fps", true);
     /* printf("%.2f %.2f\n", nav_vert_speed_fps->getFloatValue(),
               elevator_out_node->getFloatValue()); */
 
     // initialize the servo command array to central values so we don't
     // inherit junk
-    for ( int i = 0; i < 9; ++i ) {
+    for ( int i = 0; i < 8; ++i ) {
         servo_out.chn[i] = 32768;
     }
 
@@ -159,197 +160,14 @@ void control_update(short flight_mode)
     servo_out.chn[3] = 32768
         + (int16_t)(rudder_out_node->getFloatValue() * 32768);
 
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //send commands out to MNAV
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // printf("%d %d\n", servo_out[0], servo_out[1]);
-    send_servo_cmd();
+    // time stamp the packet for logging
+    servo_out.time = get_Time();
+
+    // send commanded servo positions to the MNAV
+    send_short_servo_cmd();
 }
 
 
 void control_close() {
   // nothing to see here, move along ...
 }
-
-
-#if 0
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//control code
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-void control_uav_old(bool init_done, short flight_mode)
-{
-    uint16_t servo_out[9]={0,};		 	 //elevator,aileron,throttle command
-    double  de = 0, da = 0 /*, dthr = 0*/;        //temp. variables
-    double  dthe, dphi, /* dpsi, */ dh;           //perturbed variables
-    double  dthe_ref,dphi_ref,dpsi_ref=0,dh_ref=0;//perturbed reference variable
-    double  tmpr=0,tmpr1=0,nav_psi=0;
-    double  Ps_f=0;
-    static  short anti_windup[4]={1,};
-    static short k = 0; 
-    static double Ps_f_p=0;
-
-    if ( !init_done ) {
-        // initialization:
-        servopos = servo_in;                  // save the last servo pos's
-        imuval   = imupacket;                   // save the last attitude
-        gpsval   = gpspacket;                   // save the last gps
-        navval   = navpacket;                   // save the last nav
-        Ps_f_p   = 0.0;			
-        sum[0]=sum[1]=sum[2]=sum[3]=sum[4]= 0.; // initialize integral sums
-        anti_windup[0]=anti_windup[1]=0;
-        anti_windup[2]=anti_windup[3]=0;
-        k        = 0;
-        //printf("\n[control]::control is initialized..!\n");
-    }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //obtain the purturbed states
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    dthe   = imupacket.the; 
-    dphi   = imupacket.phi;
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //pass through the first order low pass filter to remove noise
-    //G(s)=1/(tau s + 1), tau =0.4;
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Ps_f   = 0.9048*Ps_f_p + 0.09516*imupacket.Ps;
-    Ps_f_p = Ps_f;
-    dh     = Ps_f  - imuval.Ps;
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //outer-loop control: position control
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    if(numofwaypoints > 0 && whichmode[4] == 1) {
-        dpsi_ref = atan2(waypoints[k][1]-gpspacket.lon, waypoints[k][0]-gpspacket.lat);
-        //measure error
-        tmpr = sqrt( (waypoints[k][1]-gpspacket.lon)*(waypoints[k][1]-gpspacket.lon)
-                     +(waypoints[k][0]-gpspacket.lat)*(waypoints[k][0]-gpspacket.lat) );
-
-        //if error is in 15 meters
-        if(tmpr <= 2.0e-4) {
-            if (k == (numofwaypoints-1))
-                k = 0; 
-            else
-                k = k + 1;
-            
-            sum[3] = 0;
-        }
-        whichmode[2] = 1; //enable heading control
-    } else {
-        dpsi_ref = 0;
-    }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //outer-loop control: heading and altitude control
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //check whether the outer-loop controls are enabled
-    //heading
-    if ( whichmode[2] == 1 ) {
-
-        if (pos_gain[1] == 0)
-            nav_psi  = atan2(navpacket.ve, navpacket.vn);
-        else
-            nav_psi  = imupacket.psi;
-
-        //tmpr1    = sin(dpsi_ref - nav_psi);  
-        //wrap around
-        tmpr1    = wraparound(dpsi_ref - nav_psi);
-      
-        sum[3]  += tmpr1*cdT*anti_windup[2];
-
-        dphi_ref = heading_gain[0]*tmpr1
-            - heading_gain[2]*imupacket.r;
-        // FIXME: no affect in original code
-        /* + heading_gain[1]*sum[3]; */
-
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        //bound the command input
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        if(fabs(dphi_ref) > 0.61082) {
-            // 35 degrees
-            dphi_ref = sign(dphi_ref)*0.61082;
-            anti_windup[2] = 0;
-        } else {
-            anti_windup[2] = 1;
-        }
-    } else {
-        dphi_ref = 0;
-    }
-
-    //altitude
-    if (whichmode[3] == 1) {
-        dh_ref   = 0.0;
-        sum[2]  += (dh_ref - dh)*cdT*anti_windup[3];
-        dthe_ref = alt_gain[0]*(dh_ref - dh) 
-            + alt_gain[2]*gpspacket.vd;
-        // FIXME: no affect in original code
-        /* + alt_gain[1]*sum[2]; */
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        //bound the command input
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        if(fabs(dthe_ref) > 0.43630) {
-            // 25 degrees
-            dthe_ref = sign(dthe_ref)*0.43630;
-            anti_windup[3] = 0;
-        } else {
-            anti_windup[3] = 1;
-        }
-    } else {
-        dthe_ref = 0;
-    }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //inner-loop control: pitch and roll attitude control
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    sum[0]+= (dthe_ref - dthe)*cdT*anti_windup[0]; //pitch integral
-    sum[1]+= (dphi_ref - dphi)*cdT*anti_windup[1]; //roll integral
-
-   
-    de  = pitch_gain[0]*(dthe_ref - dthe)	 // radian
-        + pitch_gain[2]*(-imupacket.q)
-        + pitch_gain[1]*sum[0]
-        + pos_gain[0]*fabs(dphi);		 // compensate for the altitude loss
-    da  = roll_gain[0]*(dphi_ref - dphi)          // radian
-        + roll_gain[2]*(-imupacket.p)
-        + roll_gain[1]*sum[1];
-
-    da = -dphi;
-
-    // maximum deflection of control servos limited to 20 degrees
-    if (fabs(de) > 0.34904) {
-        de = sign(de)*0.34904;
-        anti_windup[0] = 0;
-    } else {
-        anti_windup[0] = 1;
-    }
-    //maximum deflection of control servos limited to 15 degrees
-    if (fabs(da) > 0.26178) {
-        da = sign(da)*0.26178;
-        anti_windup[1] = 0;
-    } else {
-        anti_windup[1] = 1;
-    }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //elervons:elevator and aileron mixing
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //elevator
-    servo_out[1] = servopos.chn[1] + (uint16_t)(deg2servo*(de+da)*57.3);
-    //aileron
-    servo_out[0] = servopos.chn[0] + (uint16_t)(deg2servo*(da-de)*57.3);
-    //throttle
-    servo_out[2] = servopos.chn[2];
-    // printf("servo_out[2] = %d %d %d\n", servo_out[2], servopos.chn[2], servo_in.chn[2]);
-
-    // for ( i = 0; i < 9; ++i ) {
-    //     servo_out[i] = servopos.chn[2];
-    // }
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //send commands
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    send_short_servo_cmd(servo_out);
-}
-
-#endif
