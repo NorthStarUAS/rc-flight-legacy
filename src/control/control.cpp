f4c8475e77face5ec13da927022d23a520a3d02a
Curtis L. Olson <curtolson@gmail.com>
2008-10-04 19:59:27 +0000

    - Return to "nav" based navigation versus gps. - Add a safety check to force an "idle" throttle value below 100' AGL.
    Former-commit-id: c8d796ad894fe0c91a1436adb61071028eb244c0

--- src/control/control.cpp
+++ src/control/control.cpp
@@ -47,6 +47,7 @@ struct servo servo_out;
 //static struct nav      navval;
 //enum   	      modedefs {pitch_mode,roll_mode,heading_mode,altitude_mode,speed_mode,waypoint_mode};
 
+static SGPropertyNode *agl_alt_ft_node = NULL;
 static SGPropertyNode *aileron_out_node = NULL;
 static SGPropertyNode *elevator_out_node = NULL;
 static SGPropertyNode *throttle_out_node = NULL;
@@ -70,9 +71,10 @@ void control_init() {
     ap.build();
 
     // initialize the flight control output property nodes
+    agl_alt_ft_node = fgGetNode("/position/altitude-agl-ft", true);
     aileron_out_node = fgGetNode("/controls/flight/aileron", true);
     elevator_out_node = fgGetNode("/controls/flight/elevator", true);
-    throttle_out_node = fgGetNode("/engines/engine[0]/throttle", true);
+    throttle_out_node = fgGetNode("/controls/engine/throttle", true);
     rudder_out_node = fgGetNode("/controls/flight/rudder", true);
 
     ap_target = fgGetNode("/autopilot/settings/target-roll-deg", true);
@@ -156,9 +158,57 @@ void control_update(short flight_mode)
             + (int16_t)(elevator_out_node->getFloatValue() * 32768);
     }
 
-    //throttle
+    // CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
+    // CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
+
+    // Placing the engine throttle under autopilot control requires
+    // EXTREME care!!!!
+
+    // Propellers are constructed of sharpened knife material.
+    // Electric motors don't quit and give up if they encounter intial
+    // resistance.  Severe injuries to hand or face or any other body
+    // part in the vicinity of the motor or prop can occur at any
+    // time.
+
+    // Care must be taken during initial setup, and then from that
+    // point on during all operational, testing, and ground handling
+    // phases.  Extreme vigilance must always be maintianed if the
+    // autopilot has control of the throttle.
+
+    // I cannot stress this point enough!!!  One nanosecond of
+    // distraction or loss of focus can result in severe lifelong
+    // injury or death!  Do not take your fingers or face for granted.
+    // Always maintain utmost caution and correct safety procedures to
+    // ensure safe operation with a throttle enabled UAS.
+
+    // As an internal safety measure, the throttle will be completely
+    // turned off (value of 12000 on a 0-65535 scale) when the
+    // pressure altitude is < 50m AGL.
+
+    // None of the built in safety measures are sufficient for a safe
+    // system!  Pressure sensor readings can glitch, bugs can creep
+    // into the code over time, anything can happen.  Be extremely
+    // distrustful of the propellor and always make sure your body
+    // parts are never in the path of the propellor or where the
+    // propellor and aircraft could go if the engine came alive
+    // unexpectedly.
+
+    // throttle
     servo_out.chn[2] = 32768
-        + (uint16_t)(throttle_out_node->getFloatValue() * 32768);
+        + (int16_t)(throttle_out_node->getFloatValue() * 32768);
+
+    // override and disable throttle output if within 100' of the
+    // ground (assuming ground elevation is the pressure altitude we
+    // recorded with the system started up.
+    if ( agl_alt_ft_node->getFloatValue() < 100.0 ) {
+        servo_out.chn[2] = 12000;
+    }
+
+    // printf("throttle = %.2f %d\n", throttle_out_node->getFloatValue(),
+    //        servo_out.chn[2]);
+
+    // CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
+    // CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
 
     // rudder
     servo_out.chn[3] = 32768
