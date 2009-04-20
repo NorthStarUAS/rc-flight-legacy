c93e388f78ff15e1ac93813f69aa15edfdc07cfd
Curtis L. Olson <curtolson@gmail.com>
2009-04-20 20:40:54 +0000

    - Move src/benchmarks and src/routegen to new top level directory: utils/
    - Reconcile/standardize various random usages of PI, D2R, R2D, etc.
    
    - Minor cleanups and updates to config.xml properties.
    
    
    
    Former-commit-id: bbe800390a8d4f952b97b1cc1923a32976373460

--- src/include/util.h
+++ src/include/util.h
@@ -2,13 +2,14 @@
 #define _UGEAR_UTIL_H
 
 
-#include <math.h>
+#include "include/globaldefs.h"
+
 
 // simple wrap around for -180 and + 180 
 inline double wraparound( double dta )
 {
-    if ( dta > M_PI ) dta -= M_2PI;
-    if ( dta < -M_PI ) dta += M_2PI;
+    if ( dta > SGD_PI ) dta -= SGD_2PI;
+    if ( dta < -SGD_PI ) dta += SGD_2PI;
    
     return dta;
 }	
