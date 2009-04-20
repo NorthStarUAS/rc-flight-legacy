c234ad29728860310fd1ce7b34b90df03ebbe04f
Curtis L. Olson <curtolson@gmail.com>
2009-04-20 18:38:12 +0000

    Initial revision of actuator module.

---
+++ src/actuators/act_mgr.h
@@ -0,0 +1,34 @@
+/**
+ * \file: act_mgr.h
+ *
+ * Front end management interface for output actuators
+ *
+ * Copyright (C) 2009 - Curtis L. Olson curtolson@gmail.com
+ *
+ * $Id: act_mgr.h,v 1.1 2009/04/20 18:38:12 curt Exp $
+ */
+
+
+#ifndef _UGEAR_ACT_MGR_H
+#define _UGEAR_ACT_MGR_H
+
+#include "include/globaldefs.h"
+
+enum actuator_output_t {
+    actNone,
+    actMNAV
+};
+
+//
+// global variables
+//
+
+extern struct servo servo_out;
+
+
+void Actuator_init();
+bool Actuator_update();
+void Actuator_close();
+
+
+#endif // _UGEAR_ACT_MGR_H
