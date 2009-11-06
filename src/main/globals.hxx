7cbd791696a32584ca4d17416a7e6cba6d0eae36
Curtis L. Olson <curtolson@gmail.com>
2009-11-06 19:13:26 +0000

    Release ugear_timer.cpp and globals.cxx and globals.hxx into the public domain.
    Former-commit-id: e2018c05bc76a9b1ef20e5ff51efc65273e739e3

--- src/main/globals.hxx
+++ src/main/globals.hxx
@@ -1,7 +1,20 @@
-// global references
+//
+// globals.hxx - global references
+//
+// Written by Curtis Olson, curtolson <at> gmail <dot> com.
+// Started Fall 2009.
+// This code is released into the public domain.
+// 
+
+#ifndef UG_GLOBALS_HXX
+#define UG_GLOBASL_HXX
+
 
 #include "comms/packetizer.hxx"
 
 extern UGPacketizer *packetizer;
 
 bool UGGlobals_init();
+
+
+#endif // UG_GLOBALS_HXX
