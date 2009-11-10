da67feb2cce3736bf1cd992803d35464566c133e
Curtis L. Olson <curtolson@gmail.com>
2009-11-10 16:09:40 +0000

    Clean ups to break dependencies on MNAV sensor and filter.
    Former-commit-id: c7d01356b81dd91419821748b13d659a394aa6a8

---
+++ src/main/imu_mgr.cxx
@@ -0,0 +1,137 @@
+//
+// imu_mgr.hxx - front end IMU sensor management interface
+//
+// Written by Curtis Olson, curtolson <at> gmail <dot> com.  Spring 2009.
+// This code is released into the public domain.
+// 
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "include/ugear_config.h"
+
+#include "comms/console_link.h"
+#include "comms/logging.h"
+#include "comms/packetizer.hxx"
+#include "include/globaldefs.h"
+#include "main/globals.hxx"
+#include "props/props.hxx"
+#include "util/myprof.h"
+
+#ifdef ENABLE_MNAV_SENSOR
+#  include "sensors/mnav.h"
+#endif // ENABLE_MNAV_SENSOR
+#include "sensors/ugfile.h"
+
+#include "imu_mgr.hxx"
+
+
+//
+// Global variables
+//
+
+
+void IMU_init() {
+    // traverse configured modules
+    SGPropertyNode *toplevel = fgGetNode("/config/sensors", true);
+    for ( int i = 0; i < toplevel->nChildren(); ++i ) {
+	SGPropertyNode *section = toplevel->getChild(i);
+	string name = section->getName();
+	if ( name == "imu" ) {
+	    string source = section->getChild("source")->getStringValue();
+	    string basename = "/sensors/";
+	    basename += section->getDisplayName();
+	    printf("i = %d  name = %s source = %s %s\n",
+		   i, name.c_str(), source.c_str(), basename.c_str());
+	    if ( source == "null" ) {
+		// do nothing
+	    } else if ( source == "file" ) {
+		ugfile_imu_init( basename, section );
+#ifdef ENABLE_MNAV_SENSOR
+	    } else if ( source == "mnav" ) {
+		mnav_imu_init( basename, section );
+#endif // ENABLE_MNAV_SENSOR
+	    } else {
+		printf("Unknown imu source = '%s' in config file\n",
+		       source.c_str());
+	    }
+	}
+    }
+}
+
+
+bool IMU_update() {
+    bool fresh_data = false;
+
+    // traverse configured modules
+    SGPropertyNode *toplevel = fgGetNode("/config/sensors", true);
+    for ( int i = 0; i < toplevel->nChildren(); ++i ) {
+	SGPropertyNode *section = toplevel->getChild(i);
+	string name = section->getName();
+	if ( name == "imu" ) {
+	    string source = section->getChild("source")->getStringValue();
+	    // printf("i = %d  name = %s source = %s\n",
+	    // 	   i, name.c_str(), source.c_str());
+	    if ( source == "null" ) {
+		// do nothing
+	    } else if ( source == "file" ) {
+		ugfile_read();
+		fresh_data = ugfile_get_imu();
+#ifdef ENABLE_MNAV_SENSOR
+	    } else if ( source == "mnav" ) {
+		mnav_prof.start();
+		// read IMU until no data available.  This will flush any
+		// potential backlog that could accumulate for any reason.
+		mnav_start_nonblock_read();
+		while ( mnav_read_nonblock() );
+		mnav_prof.stop();
+		fresh_data = mnav_get_imu();
+#endif // ENABLE_MNAV_SENSOR
+	    } else {
+		printf("Unknown imu source = '%s' in config file\n",
+		       source.c_str());
+	    }
+	}
+    }
+
+    if ( fresh_data ) {
+	if ( console_link_on || log_to_file ) {
+	    uint8_t buf[256];
+	    int size = packetizer->packetize_imu( buf );
+
+	    if ( console_link_on ) {
+		console_link_imu( buf, size );
+	    }
+
+	    if ( log_to_file ) {
+		log_imu( buf, size );
+	    }
+	}
+    }
+
+    return fresh_data;
+}
+
+
+void IMU_close() {
+    // traverse configured modules
+    SGPropertyNode *toplevel = fgGetNode("/config/sensors", true);
+    for ( int i = 0; i < toplevel->nChildren(); ++i ) {
+	SGPropertyNode *section = toplevel->getChild(i);
+	string name = section->getName();
+	if ( name == "imu" ) {
+	    string source = section->getChild("source")->getStringValue();
+	    printf("i = %d  name = %s source = %s\n",
+		   i, name.c_str(), source.c_str());
+	    if ( source == "file" ) {
+		ugfile_close();
+	    } else if ( source == "mnav" ) {
+		// nop
+	    } else {
+		printf("Unknown imu source = '%s' in config file\n",
+		       source.c_str());
+	    }
+	}
+    }
+}
