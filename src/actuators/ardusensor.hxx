dae4b1fbad461543f600e5dfafa534584b97fa75
Curtis L. Olson <curtolson@gmail.com>
2010-05-24 20:46:36 +0000

    Rename arduservo -> ardusensor (I don't like either name.) Add support for 4 analog inputs on the ardupilot err sensor err servo thing. Research an initial function to convert ADC values to airspeed (kts) Create an "air-data" module for ardusensor
    Former-commit-id: 702fb3d2bf3d2e90ebeb474f6bb24fa4a67b6fe5

---
+++ src/actuators/ardusensor.hxx
@@ -0,0 +1,23 @@
+//
+// FILE: ardusensor.hxx
+// DESCRIPTION: interact with ardupilot based servo subsystem board
+//
+
+#ifndef _UGEAR_ARDUSENSOR_HXX
+#define _UGEAR_ARDUSENSOR_HXX
+
+
+#include "include/globaldefs.h"
+
+#include "props/props.hxx"
+
+
+// function prototypes
+bool ardusensor_init( SGPropertyNode *config );
+bool ardusensor_update();
+void ardusensor_close();
+
+bool ardusensor_airdata_init( string rootname );
+bool ardusensor_airdata_update();
+
+#endif // _UGEAR_ARDUSENSOR_HXX
