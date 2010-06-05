b193e8ddeed30a85262d4fb7e36fcb762347716f
Curtis L. Olson <curtolson@gmail.com>
2010-06-05 18:10:15 +0000

    Initial revision.
    Former-commit-id: 454e706f8e057d683e84c5cbba36828b41e0fe46

---
+++ setpath-gumstix-oe.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Uncomment one of the following lines and adjust the path for your local
+# installation locations.
+
+# GumStix "Open Embedded" build environment
+export PATH=${PATH}:/home/curt/Projects/GumStixOE/gumstix-oe/tmp/cross/bin
+
+
