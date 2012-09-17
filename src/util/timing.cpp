47b8bc42ff51e681075a05db2343c8f09d42d256
Curtis L. Olson <curt0001@flightgear.org>
2012-09-17 10:14:15 -0500

    Updated admin utils and readme's. Created a "stopme" utility requiring you to type the magic phrase within "n"   seconds in order to pass the test. Removed some old unused cruft from utils/timing.cpp
    Former-commit-id: 7f386e2d3ef3fb62cf17c0f6fb7cf4b2ba4e5b75

--- src/util/timing.cpp
+++ src/util/timing.cpp
@@ -11,59 +11,7 @@
 #include <unistd.h>
 #include <time.h>
 
-#include "comms/display.h"
-
 #include "include/globaldefs.h"
-
-//global variables
-static double exe_rate[5];
-static int count[5] = {0,};
-static struct timespec ts_p[5];
-static double sum[5] = {0.,};
-
-//
-// snap time interval
-//
-void snap_time_interval( const char *threadname, int displaytime, short id )
-{
-    struct timespec	   ts;
-    double 		   sec, nsec, dt, rate;
-    
-    clock_gettime(CLOCK_REALTIME, &ts);
-    sec     = ts.tv_sec - ts_p[id].tv_sec;
-    nsec    = ts.tv_nsec- ts_p[id].tv_nsec;
-    dt = sec + nsec*1.0e-9;
-    ts_p[id]= ts;
-    sum[id] += dt;
-	
-    if (++count[id] == displaytime) {
-        rate = sum[id] / count[id];
-        exe_rate[id] = rate;
-
-        if ( display_on ) {
-            printf("[%s]: For %.1f sec: updates ran at %.2f(hz): %.2f(ms) step\n",
-                   threadname, sum[id], 1/rate, rate*1000);
-        }
-        sum[id] = 0;
-        count[id] = 0;
-    }
-}
-
-double get_time_interval( short id )
-{
-    struct timespec		ts;
-    static struct timespec  ts_p[5];
-    double 			nsec,elapsed;
-
-    clock_gettime(CLOCK_REALTIME, &ts);
-    nsec    = ts.tv_nsec- ts_p[id].tv_nsec;
-    elapsed = nsec*1.0e-9;
-        
-    if(elapsed <=0) elapsed =0.0;        
-    ts_p[id]= ts;
-       
-    return elapsed;	
-}	
 
 
 double get_Time( bool reset )
