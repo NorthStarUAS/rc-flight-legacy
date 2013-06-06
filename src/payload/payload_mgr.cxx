1ccdc8fdea9a967c3b4beb18d83f5980ea991445
Curtis L. Olson <curtolson@flightgear.org>
2013-06-06 14:02:38 -0500

    Add initial payload manager class.  For now this is just there as a central place holder to handle logging and remote communication of payload status. Longer term maybe we'll add more complex payload mangement functionality here.
    Former-commit-id: b7a9587da9673145ddae4ad498ad2899db48ed94

---
+++ src/payload/payload_mgr.cxx
@@ -0,0 +1,76 @@
+/**
+ * \file: payload_mgr.cpp
+ *
+ * Payload manager
+ *
+ * Copyright (C) 2011 - Curtis L. Olson curtolson@gmail.com
+ *
+ * $Id: act_mgr.cpp,v 1.3 2009/08/25 15:04:01 curt Exp $
+ */
+
+#include <cstdio>
+
+#include "include/ugear_config.h"
+
+#include "comms/display.h"
+#include "comms/logging.h"
+#include "comms/remote_link.h"
+#include "main/globals.hxx"
+#include "props/props.hxx"
+
+#include "payload_mgr.hxx"
+
+
+// comm property nodes
+static SGPropertyNode *payload_console_skip = NULL;
+static SGPropertyNode *payload_logging_skip = NULL;
+
+
+UGPayloadMgr::UGPayloadMgr()
+{
+}
+
+UGPayloadMgr::~UGPayloadMgr() {
+}
+
+
+
+void UGPayloadMgr::bind() {
+    // config_props = fgGetNode( "/config/payload", true );
+
+    // initialize comm nodes
+    payload_console_skip = fgGetNode("/config/remote-link/payload-skip", true);
+    payload_logging_skip = fgGetNode("/config/logging/payload-skip", true);
+}
+
+
+void UGPayloadMgr::init() {
+    bind();
+}
+
+
+bool UGPayloadMgr::update() {
+    if ( remote_link_on || log_to_file ) {
+	uint8_t buf[256];
+	int size = packetizer->packetize_payload( buf );
+
+	if ( remote_link_on ) {
+	    // printf("sending filter packet\n");
+	    remote_link_payload( buf, size,
+				 payload_console_skip->getIntValue() );
+	}
+
+	if ( log_to_file ) {
+	    log_airdata( buf, size, payload_logging_skip->getIntValue() );
+	}
+    }
+    return true;
+}
+
+
+void UGPayloadMgr::close() {
+}
+
+
+// global instance of the payload manager
+UGPayloadMgr payload_mgr;
