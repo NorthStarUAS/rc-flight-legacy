ad76f7d1efb73d364dbec1a2244b0f74615a507b
Curtis L. Olson <curtolson@flightgear.org>
2013-10-14 11:33:23 -0500

    Remove old dependency on point3d and polar3d and used updated SGMath routines.
    Former-commit-id: f8c6042588d57b0878109de927e5b5351ee2d650

---
+++ utils/routegen/routegen.cpp
@@ -0,0 +1,394 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <string>
+
+#include <control/route.hxx>
+#include <math/SGMath.hxx>
+
+#include <include/globaldefs.h>
+
+using std::string;
+
+double wind_dir = 0.0;
+double wind_speed = 0.0;
+
+
+void usage( char *prog ) {
+    printf("Usage: %s [ --options ]\n", prog);
+    printf("\n");
+    printf("Note: coordinates can be specified in the following formats:\n");
+    printf("  Decimal degrees: -158.25929 or decimal minutes: W158:15.5574\n");
+    printf("\n");
+    printf("  --start-lon <lon_coord>\n");
+    printf("  --start-lat <lat_coord>\n");
+    printf("  --home-lon <lon_coord>\n");
+    printf("  --home-lat <lat_coord>\n");
+    printf("  --route-alt <altitude_ft>\n");
+    printf("  --swath-heading <initial swath heading deg true>\n");
+    printf("  --swath-length <swath length meters>\n");
+    printf("  --swath-width <width between swaths in meters>\n");
+    printf("  --cross-heading <cross angle>\n");
+    printf("         ideally this is into the wind and 90 degrees from the swath heading>\n");
+    printf("  --wind-from-deg <wind 'from' direction>\n");
+    printf("  --wind-speed-kts <wind speed kts>\n");
+    printf("  --flight-speed-kts <estimated air speed kts>\n");
+    printf("  --flight-time-min <estimated safe flight time in minutes>\n");
+
+    exit(0);
+}
+
+
+double get_dist( const SGGeoc start, const SGGeoc dest ) {
+    return SGGeodesy::distanceM( start, dest );
+}
+
+
+double get_course( const SGGeoc start, const SGGeoc dest ) {
+    return SGGeodesy::courseRad( start, dest );
+}
+
+
+double parse_coord( const string coord ) {
+    double result = 0.0;
+
+    printf("parse_coord: %s\n", coord.c_str());
+
+    if ( !coord.length() ) {
+        printf("Parse error: zero length coord\n");
+        return result;
+    }
+
+    char c = coord.at(0);
+    if ( (c >= '0' && c <= '9') || c == '-' ) {
+        // decimal degree
+        result = atof( coord.c_str() );
+    } else {
+        int sign = 1;
+        if ( c == 's' || c == 'S' || c == 'w' || c == 'W' ) {
+            sign = -1;
+        }
+        string mindeg = coord.substr(1);
+        string::size_type pos = mindeg.find_first_of(":");
+        if ( pos == string::npos ) {
+            printf("whole degrees\n");
+            result = atof( mindeg.c_str() );
+        } else {
+            printf("degree-minute.decimal\n");
+            string deg = mindeg.substr(0, pos);
+            string min = mindeg.substr(pos + 1);
+            printf("deg = %s  min = %s\n", deg.c_str(), min.c_str());
+            result = atof(deg.c_str()) + atof(min.c_str())/60.0;
+        }
+        result *= sign;
+    }
+
+    printf("result = %f\n", result);
+    return result;
+}
+
+
+double calc_ete_min(double course, double dist_m, double flight_speed,
+                    double wind_speed, double wind_from_deg) 
+{
+    if ( dist_m < 0.001 ) {
+        return 0.0;
+    }
+
+    double wind_dir = wind_from_deg * SGD_DEGREES_TO_RADIANS;
+    double swc = (wind_speed/flight_speed) * sin(wind_dir-course);
+    double gs = 0.0;
+    if ( fabs(swc) >= 1.0 ) {
+        printf("course cannot be flown-- wind too strong\n");
+        exit(-1);
+    } else {
+        double heading = course + asin(swc);
+        if ( heading < 0.0 ) { heading = heading + SGD_2PI; }
+        if ( heading > SGD_2PI ) { heading = heading - SGD_2PI; }
+        gs = flight_speed * sqrt(1-swc*swc) - wind_speed * cos(wind_dir-course);
+        if ( gs <= 0.0 ) {
+            printf("course cannot be flown-- wind too strong\n");
+            exit(-1);
+        }
+    }
+
+    double dist_nm = dist_m * SG_METER_TO_NM;
+    double ete_hour = dist_nm / gs;
+    double ete_min = ete_hour * 60;
+
+    printf("course = %.0f dist = %.0f tas = %.0f ws = %.0f winddir = %.0f\n",
+           course * SGD_RADIANS_TO_DEGREES, dist_m, flight_speed,
+           wind_speed, wind_from_deg);
+    printf("  ete_min = %.2f\n", ete_min);
+
+    return ete_min;
+}
+
+double route_length( const SGRoute route ) {
+    // assumptions: flight is launched from "home" coordinates, UAS flies the
+    // route.  The last waypoint in the route is the "home" location.
+
+    double result = 0.0;
+    SGWayPoint wpt;
+    SGGeoc current, next;
+
+    // launch pt (aka home) location is the last waypoint
+    int size = route.size();
+    wpt = route.get_waypoint( size - 1 );
+
+    current.setLongitudeDeg( wpt.get_target_lon() );
+    current.setLatitudeDeg( wpt.get_target_lat() );
+
+    for ( int i = 0; i < size; ++i ) {
+        wpt = route.get_waypoint( i );
+	next.setLongitudeDeg( wpt.get_target_lon() );
+	next.setLatitudeDeg( wpt.get_target_lat() );
+        // printf("%.5f %.5f  %.5f %.5f\n", current.lon(), current.lat(),
+        //        next.lon(), next.lat());
+        double dist = SGGeodesy::distanceM( current, next );
+
+        result += dist;
+        current = next;
+    }
+
+    return result;
+}
+
+
+double route_max_dist( const SGRoute route ) {
+    // Assumption: the last waypoint in the route is the home/launch location.
+
+    double maxdist = 0.0;
+    SGWayPoint wpt;
+    SGGeoc home, current;
+
+    // launch pt (aka home) location is the last waypoint
+    int size = route.size();
+    wpt = route.get_waypoint( size - 1 );
+    home.setLongitudeDeg( wpt.get_target_lon() );
+    home.setLatitudeDeg( wpt.get_target_lat() );
+
+    for ( int i = 0; i < size; ++i ) {
+        wpt = route.get_waypoint( i );
+	current.setLongitudeDeg( wpt.get_target_lon() );
+	current.setLatitudeDeg( wpt.get_target_lat() );
+        // printf("%.5f %.5f  %.5f %.5f\n", current.lon(), current.lat(),
+        //        next.lon(), next.lat());
+	double dist = SGGeodesy::distanceM( home, current );
+        // printf("  course = %.2f  dist = %.2f\n", course, dist);
+
+        if ( dist > maxdist ) { maxdist = dist; }
+    }
+
+    return maxdist;
+}
+
+
+int main( int argc, char **argv )
+{
+    double start_lon = 0.0;
+    double start_lat = 0.0;
+    double home_lon = 0.0;
+    double home_lat = 0.0;
+    double route_alt_m = 0.0;
+    double swath_hdg = 0.0;
+    double swath_len = 0.0;
+    double swath_width = 0.0;
+    double cross_hdg = 0.0;
+    double flight_speed = 0.0;
+    double flight_time = 0.0;
+    
+    // Parse the command line
+    for ( int iarg = 1; iarg < argc; iarg++ ) {
+        if ( !strcmp(argv[iarg], "--start-lon" )  ) {
+            ++iarg;
+            // start_lon = atof( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+            start_lon = parse_coord( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--start-lat") ) {
+            ++iarg;
+            start_lat = parse_coord( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--home-lon") ) {
+            ++iarg;
+            home_lon = parse_coord( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--home-lat") ) {
+            ++iarg;
+            home_lat = parse_coord( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--route-alt") ) {
+            ++iarg;
+            route_alt_m = atof( argv[iarg] ) * SG_FEET_TO_METER;
+        } else if ( !strcmp(argv[iarg],"--swath-heading") ) {
+            ++iarg;
+            swath_hdg = SGD_2PI - atof( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--swath-length") ) {
+            ++iarg;
+            swath_len = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg],"--swath-width") ) {
+            ++iarg;
+            swath_width = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg],"--cross-heading") ) {
+            ++iarg;
+            cross_hdg = SGD_2PI - atof( argv[iarg] ) * SGD_DEGREES_TO_RADIANS;
+        } else if ( !strcmp(argv[iarg],"--wind-from-deg") ) {
+            ++iarg;
+            wind_dir = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg],"--wind-speed-kts") ) {
+            ++iarg;
+            wind_speed = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg],"--flight-speed-kts") ) {
+            ++iarg;
+            flight_speed = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg],"--flight-time-min") ) {
+            ++iarg;
+            flight_time = atof( argv[iarg] );
+        } else if ( !strcmp(argv[iarg], "--help") ) {
+            usage( argv[0] );
+        } else {
+            printf("Unknown option \"%s\"\n\n", argv[iarg]);
+            usage( argv[0] );
+        }
+    }
+
+    if ( route_alt_m < 1.0 ) {
+        printf("Error: no route altitude specified\n");
+        exit(-1);
+    }
+
+    // Generate route waypoints
+    SGRoute route;
+    double time_remaining = flight_time;
+    double ete_home = 0.0;
+    int state = 0;
+
+    SGGeoc current;
+    current.setLongitudeDeg( start_lon );
+    current.setLatitudeDeg( start_lat );
+    route.add_waypoint( SGWayPoint(current.getLongitudeDeg(),
+				   current.getLatitudeDeg(),
+				   route_alt_m) );
+
+    // subtract out distance from launch/recover point (aka home) to
+    // first waypoint
+    SGGeoc home;
+    home.setLongitudeDeg( home_lon );
+    home.setLatitudeDeg( home_lat );
+    double course, dist, ete;
+    dist = SGGeodesy::distanceM( current, home );
+    course =SGGeodesy::courseRad( current, home ) * SG_RADIANS_TO_DEGREES;
+    ete = calc_ete_min(course, dist, flight_speed, wind_speed, wind_dir);
+    time_remaining -= ete;
+    printf("time remaining = %f\n", time_remaining);
+
+    while ( true ) {
+        // once we determine the next leg distance below, we can
+        // decide if it is time to break out of this loop
+        
+        double hdg = swath_hdg;
+        double dist = swath_len;
+        switch ( state ) {
+        case 0:
+            hdg = swath_hdg;
+            dist = swath_len;
+            break;
+        case 1:
+            hdg = cross_hdg;
+            dist = swath_width;
+            break;
+        case 2:
+            hdg = (swath_hdg - SGD_PI);
+            if ( hdg < 0.0 ) { hdg += SGD_2PI; }
+            dist = swath_len;
+            break;
+        case 3:
+            hdg = cross_hdg;
+            dist = swath_width;
+            break;
+        default:
+            break;
+        }
+
+        // compute the next waypoint
+	SGGeoc next;
+	SGGeodesy::advanceRadM( current, hdg * SGD_DEGREES_TO_RADIANS,
+				dist, next );
+        ete = calc_ete_min(hdg, dist, flight_speed, wind_speed, wind_dir);
+
+        // compute distance to return home
+        double dist_home = SGGeodesy::distanceM( next, home );
+	course = SGGeodesy::courseRad( next, home ) * SG_RADIANS_TO_DEGREES;
+        ete_home = calc_ete_min(course, dist_home, flight_speed,
+                                wind_speed, wind_dir);
+
+        // finish the loop if we've run out of flight time
+        if ( ete_home + ete > time_remaining ) {
+            break;
+        }
+
+        route.add_waypoint( SGWayPoint(next.getLongitudeDeg(), next.getLatitudeDeg(), route_alt_m) );
+        
+        current = next;
+        time_remaining -= ete;
+
+        state = (state + 1) % 4;
+    }
+    route.add_waypoint( SGWayPoint(home_lon, home_lat, route_alt_m) );
+
+    printf("\n");
+    printf("Route summary:\n");
+    printf("  waypoints: %d\n", route.size());
+    printf("  route length: %.0f meters\n", route_length( route ) );
+    printf("  max distance from home: %.0f meters\n", route_max_dist( route ) );
+
+    int size = route.size();
+    int i;
+    FILE *fd;
+
+    // Output route for gnuplot
+    fd = fopen("route.gnuplot", "w");
+    for ( i = 0; i < size; ++i ) {
+        SGWayPoint wpt = route.get_waypoint( i );
+        fprintf(fd, "%.8f %.8f\n",
+               wpt.get_target_lon() * SGD_RADIANS_TO_DEGREES,
+               wpt.get_target_lat() * SGD_RADIANS_TO_DEGREES );
+    }
+    fclose(fd);
+    
+    // Output route for microgear
+    fd = fopen("route.xml", "w");
+    fprintf(fd, "<?xml version=\"1.0\"?>\n");
+    fprintf(fd, "<PropertyList>\n");
+    for ( i = 0; i < size; ++i ) {
+        SGWayPoint wpt = route.get_waypoint( i );
+        fprintf(fd, "  <wpt>\n");
+        fprintf(fd, "    <lat>%.8f</lat>\n",
+                wpt.get_target_lat() * SGD_RADIANS_TO_DEGREES );
+        fprintf(fd, "    <lon>%.8f</lon>\n",
+                wpt.get_target_lon() * SGD_RADIANS_TO_DEGREES );
+        fprintf(fd, "    <alt-ft>%.0f</alt-ft>\n",
+                wpt.get_target_alt_m() * SG_METER_TO_FEET );
+        fprintf(fd, "  </wpt>\n");
+    }
+    fprintf(fd, "</PropertyList>\n");
+    fclose(fd);
+    
+    // Output route for lfstech glass
+    fd = fopen("route.pln", "w");
+    for ( i = 0; i < size; ++i ) {
+        SGWayPoint wpt = route.get_waypoint( i );
+        fprintf(fd, "GEO %.8f %.8f WP%02d %.1f\n",
+                wpt.get_target_lat() * SGD_RADIANS_TO_DEGREES,
+                wpt.get_target_lon() * SGD_RADIANS_TO_DEGREES,
+                i,
+                wpt.get_target_alt_m() * SG_METER_TO_FEET
+          );
+    }
+    fprintf(fd, "END\n");
+    fclose(fd);
+
+    return 0;
+}
+
+
+
+
+
