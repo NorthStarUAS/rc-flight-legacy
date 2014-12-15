5d9ee344e1459e0535a71ce1e0efa45d476741b7
Curtis L. Olson <curtolson@flightgear.org>
2014-12-15 17:45:16 -0600

    Initial revision of a python based auto home updater.  This still works with gpsd as the gps driver, but should handle gps's with any update rate. Also it makes an attempt to not update heading/track if the average velocity is below some threshold (0.5 mps right now.)  This should help avoid situations where the uav route pattern alignment changes randomly from random gps walking (even though the base station is really stationary.)
    Former-commit-id: a7cbf58a8e87358d1eaa3da43f15fdca28fc2662

---
+++ utils/dynamichome/autohome.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+from gps import *
+import math
+import time
+import fgtelnet
+
+host = "localhost"
+port = 5402
+
+report_time = time.time()
+interval = 30.0
+deg2rad = math.pi / 180.0
+
+x_sum = 0.0
+y_sum = 0.0
+counter = 0
+#filt_x = 0.0
+#filt_y = 0.0
+#filt_speed = 0.0
+track = 0.0
+
+gpsd = gps(mode=WATCH_ENABLE)
+try:
+    while True:
+        # read next available gps message
+        report = gpsd.next()
+        
+        # Check report class for 'DEVICE' messages from gpsd.  If
+        # we're expecting messages from multiple devices we should
+        # inspect the message to determine which device
+        # has just become available.  But if we're just listening
+        # to a single device, this may do.
+        if report['class'] == 'DEVICE':
+            # Clean up our current connection.
+            gpsd.close()
+            # Tell gpsd we're ready to receive messages.
+            gpsd = gps(mode=WATCH_ENABLE)
+
+        # average track (x, y components)
+        if math.isnan(gpsd.fix.track) or math.isnan(gpsd.fix.speed):
+            # skip track work
+            a = 1
+        else:
+            track_rad = math.pi / 2.0 - gpsd.fix.track * deg2rad
+            x = math.cos(track_rad) * gpsd.fix.speed;
+            y = math.sin(track_rad) * gpsd.fix.speed;
+            x_sum += x
+            y_sum += y
+            counter += 1
+            #filt_x = 0.9 * filt_x + 0.1 * x;
+            #filt_y = 0.9 * filt_y + 0.1 * y;
+            filt_speed = math.sqrt( x*x + y*y )
+            print "hdg = %.1f rad = %.2f x = %.2f  y = %.2f spd = %.2f\n" % (gpsd.fix.track, track_rad, x, y, filt_speed)
+
+        # send home updates at specified interval
+        cur_time = time.time()
+        if cur_time >= report_time + interval:
+            report_time = cur_time
+            
+            print
+            print ' GPS reading'
+            print '----------------------------------------'
+            print 'latitude    ' , gpsd.fix.latitude
+            print 'longitude   ' , gpsd.fix.longitude
+            print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+            print 'altitude (m)' , gpsd.fix.altitude
+            print 'eps         ' , gpsd.fix.eps
+            print 'epx         ' , gpsd.fix.epx
+            print 'epv         ' , gpsd.fix.epv
+            print 'ept         ' , gpsd.fix.ept
+            print 'speed (m/s) ' , gpsd.fix.speed
+            print 'climb       ' , gpsd.fix.climb
+            print 'track       ' , gpsd.fix.track
+            print 'mode        ' , gpsd.fix.mode
+            print
+            print 'sats        ' , gpsd.satellites
+
+            # update movement average/stats
+            if counter > 0:
+                x = x_sum / counter
+                y = y_sum / counter
+            else:
+                x = 0.0
+                y = 0.0
+            print "x_sum = %.2f y_sum = %.2f counter = %d" % (x_sum, y_sum, counter)
+            # zero accumulators
+            x_sum = 0.0
+            y_sum = 0.0
+            counter = 0
+
+            avg_speed = math.sqrt( x*x + y*y )
+            if avg_speed > 0.5:
+                # Only update track if speed is above a threshold of movement.
+                # This way we don't get random orientations if the base station
+                # isn't moving.  Note: 0.5 mps =~ 1 kt
+                track = 90.0 - math.atan2( y, x ) / deg2rad
+            print "avg track = %.1f x = %.2f  y = %.2f spd = %.2f\n" % (track, x, y, avg_speed)
+
+
+            if math.isnan(gpsd.fix.longitude) or math.isnan(gpsd.fix.latitude):
+                # sanity check on our position
+                continue
+                
+            message = "send home,%.8f,%.8f,0,%.1f" % \
+                      ( gpsd.fix.longitude, gpsd.fix.latitude, track)
+            print message
+            try:
+                t = fgtelnet.FGTelnet(host, port)
+                t.send("data")
+                t.send("message")
+                t.quit()
+            except:
+                print "Cannot connect to uglink"
+
+                    
+except StopIteration:
+    print "GPSD has terminated"
