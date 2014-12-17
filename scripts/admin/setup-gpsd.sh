5cfa739b893914ecf31ae3ba7af813d383ec12c8
root <root@omap3-multi.(none)>
2014-12-17 16:20:42 +0000

    Updates to autohome and startup script for installation.
    Former-commit-id: 44ec3623252af6c77622bf45fe91365b0e33afbe

--- scripts/admin/setup-gpsd.sh
+++ scripts/admin/setup-gpsd.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-GPS_PORT=/dev/ttyUSB0
-LOG_FILE=/var/log/dynamichome.log
+#GPS_PORT=/dev/ttyUSB0
+GPS_PORT=/dev/ttyO0
+
+LOG_FILE=/var/log/autohome.log
 
 if [ -c $GPS_PORT ]; then
     echo "Starting gpsd..."
     gpsd $GPS_PORT
     sleep 1
-    echo "Starting dynamic home updater..."
-    dynamichome >& $LOG_FILE 2>&1 &
+    echo "Starting automatic home updater..."
+    autohome.py >& $LOG_FILE 2>&1 &
 else
     echo "No $GPS_PORT detected..."
 fi
