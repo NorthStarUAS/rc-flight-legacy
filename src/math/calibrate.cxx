bc1bc3e95a18010f5a348e50bb5d03ce337d612e
Curtis L. Olson <curtolson@flightgear.org>
2015-05-04 12:35:26 -0500

    Initial commit of a new calibration class which abstacts the process of maintaining 2nd degree polygon interpolated fit function for bias and scale vs. temperature and can return the actual bias and scale values or can convert a value given a tempature.

---
+++ src/math/calibrate.cxx
@@ -0,0 +1,93 @@
+/**
+ * \file: calibrate.cxx
+ *
+ * Callibration helper class
+ *
+ * Copyright (C) 2015 - Curtis L. Olson curtolson@gmail.com
+ *
+ */
+
+#include <stdio.h>
+
+#include "util/strutils.hxx"
+
+#include "calibrate.hxx"
+
+// set parameters to default zero bias and 1.0 scaling factor
+void UGCalibrate::defaults()
+{
+    bias[0] = 0.0;
+    bias[1] = 0.0;
+    bias[2] = 0.0;
+    scale[0] = 0.0;
+    scale[1] = 0.0;
+    scale[2] = 1.0;
+}
+
+
+UGCalibrate::UGCalibrate()
+{
+    defaults();
+}
+
+
+UGCalibrate::~UGCalibrate()
+{
+    // nothing to do
+}
+
+
+// load parameters from specified property subtree
+void UGCalibrate::init( SGPropertyNode *config )
+{
+    defaults();
+    
+    SGPropertyNode *node = NULL;
+    
+    node = config->getChild("bias");
+    if ( node != NULL ) {
+	string bias_str = node->getStringValue();
+	vector<string> tokens = split( bias_str );
+	if ( tokens.size() == 1 ) {
+	    // constant bias
+	    bias[0] = 0.0;
+	    bias[1] = 0.0;
+	    bias[2] = atof( tokens[0].c_str() );
+	} else if ( tokens.size() == 3 ) {
+	    // 2nd degree polynomial fit
+	    bias[0] = atof( tokens[0].c_str() );
+	    bias[1] = atof( tokens[1].c_str() );
+	    bias[2] = atof( tokens[2].c_str() );
+	} else {
+	    // error, set zero biase
+	    bias[0] = 0.0;
+	    bias[1] = 0.0;
+	    bias[2] = 0.0;
+	}
+    }
+    node = config->getChild("scale");
+    if ( node != NULL ) {
+	string scale_str = node->getStringValue();
+	vector<string> tokens = split( scale_str );
+	if ( tokens.size() == 1 ) {
+	    // constant scale
+	    scale[0] = 0.0;
+	    scale[1] = 0.0;
+	    scale[2] = atof( tokens[0].c_str() );
+	} else if ( tokens.size() == 3 ) {
+	    // 2nd degree polynomial fit
+	    scale[0] = atof( tokens[0].c_str() );
+	    scale[1] = atof( tokens[1].c_str() );
+	    scale[2] = atof( tokens[2].c_str() );
+	} else {
+	    // error, set 1.0 scale
+	    scale[0] = 0.0;
+	    scale[1] = 0.0;
+	    scale[2] = 1.0;
+	}
+    }
+    // printf("bias = %.6f %.6f %.6f, scale = %.4f\n",
+    //        bias[0], bias[1], bias[2], scale);
+}
+
+
