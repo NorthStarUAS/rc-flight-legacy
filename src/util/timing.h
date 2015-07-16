b069598644751a2977e650de6b8ad49ad0dad5cd
Curtis L. Olson <curtolson@flightgear.org>
2015-07-16 06:36:22 -0500

    Update header #define name.

--- src/util/timing.h
+++ src/util/timing.h
@@ -1,5 +1,5 @@
-#ifndef _UGEAR_MISC_H
-#define _UGEAR_MISC_H
+#ifndef _UGEAR_TIMING_H
+#define _UGEAR_TIMING_H
 
 
 extern void snap_time_interval( const char *threadname,
@@ -8,4 +8,4 @@ extern double get_time_interval( short id );
 extern double get_Time();
 extern double get_RealTime();
 
-#endif // _UGEAR_MISC_H
+#endif // _UGEAR_TIMING_H
