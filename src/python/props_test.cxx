898fb3562b1878d76f8e5a8dda8de00497f42c1e
Curtis L. Olson <olson126@umn.edu>
2016-01-05 14:37:07 -0600

    Updated to test new pyprops function.

--- src/python/props_test.cxx
+++ src/python/props_test.cxx
@@ -2,7 +2,13 @@
 
 int main(int argc, char **argv) {
     pyPropsInit(argc, argv);
-    pyPropertyNode *abc;
-    pyGetNode("/sensors/imu[2]/accel/az");
+
+    pyPropertyNode az_node = pyGetNode("/sensors/imu[2]/accel/az");
+    double az = az_node.getDoubleValue();
+    printf("az = %.2f\n", az);
+    
+    pyGetNode("/sensors/imu[2]/accel");
+    pyGetNode("/sensors/imu[2]");
+    pyGetNode("/sensors/imu");
     pyPropsClose();
 }
