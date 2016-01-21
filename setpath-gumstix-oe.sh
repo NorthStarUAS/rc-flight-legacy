f4b7ac4616f469b4c5c3ba5e68c5ec958461dcc9
Curtis L. Olson <olson126@umn.edu>
2016-01-21 13:40:38 -0600

    Remove some deprecated gumstix/verdex/overo information (these files still exist in the fgprops branch.)

--- setpath-gumstix-oe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Uncomment one of the following lines and adjust the path for your local
-# installation locations.
-
-# GumStix "Open Embedded" build environment
-export PATH=${PATH}:/home/curt/Projects/GumStixOE/gumstix-oe/tmp/cross/bin
-
-
