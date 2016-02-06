7452005712e8f59290e638161f7464b73dedbcf7
Curtis L. Olson <curtolson@flightgear.org>
2016-02-06 05:49:36 -0600

    Add function that computes a destination lon/lat given a starting lon/lat, course, distance.

--- src/mission/greatcircle.py
+++ src/mission/greatcircle.py
@@ -3,14 +3,15 @@
 import math
 
 EPS = 0.0001
-
 d2r = math.pi / 180.0
 r2d = 180.0 / math.pi
 rad2nm = (180.0 * 60.0) / math.pi
+nm2rad = 1.0 / rad2nm
 nm2meter = 1852
+meter2nm = 1.0 / nm2meter
 
-# p1 = (lat1, lon1)
-# p2 = (lat2, lon2)
+# p1 = (lat1(deg), lon1(deg))
+# p2 = (lat2(deg), lon2(deg))
 def course_and_dist(p1, p2):
     # this formulations uses postive lon = W (opposite of usual, so we
     # invert the longitude.)
@@ -43,3 +44,15 @@ def course_and_dist(p1, p2):
     tc1_deg = tc1_rad * r2d
 
     return (tc1_deg, dist_m)
+
+def project_course_distance(p1, course_deg, dist_m):
+    lat1 = p1[0] * d2r
+    lon1 = -p1[1] * d2r
+    tc = course_deg * d2r
+    d = dist_m * meter2nm * nm2rad
+    lat = math.asin(math.sin(lat1)*math.cos(d)+math.cos(lat1)*math.sin(d)*math.cos(tc))
+    if math.cos(lat) < EPS:
+        lon = lon1      # endpoint a pole
+    else:
+        lon = math.fmod(lon1-math.asin(math.sin(tc)*math.sin(d)/math.cos(lat))+math.pi, 2*math.pi) - math.pi
+    return (lat*r2d, -lon*r2d)
