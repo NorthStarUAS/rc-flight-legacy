f3354123ade658f2bbf61990270123617cbc98ef
Curtis L. Olson <curtolson@flightgear.org>
2016-02-24 18:07:43 -0600

    Initial revision of an asyncronous network server (for telnet connectsions) this is the approach used by medusa and the plib net* library.  Surprise: already cooked into python!

---
+++ auralink/telnet.py
@@ -0,0 +1,42 @@
+# asynchat example from here:
+#   http://www.grantjenks.com/wiki/random/python_asynchat_chat_example
+
+import asynchat
+import asyncore
+import socket
+ 
+class ChatHandler(asynchat.async_chat):
+    def __init__(self, sock):
+        asynchat.async_chat.__init__(self, sock=sock)
+ 
+        self.set_terminator('\n')
+        self.buffer = []
+ 
+    def collect_incoming_data(self, data):
+        self.buffer.append(data)
+ 
+    def found_terminator(self):
+        msg = ''.join(self.buffer)
+        print 'Received:', msg
+        self.push(msg + '\n')
+        self.buffer = []
+ 
+class ChatServer(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind((host, port))
+        self.listen(5)
+ 
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = ChatHandler(sock)
+ 
+server = ChatServer('localhost', 5050)
+ 
+print 'Serving on localhost:5050'
+while True:
+    asyncore.loop(count=1)
