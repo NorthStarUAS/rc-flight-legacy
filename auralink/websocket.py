5646a571a36e5c75e3882b7645f13ba9842ca862
Curtis L. Olson <olson126@umn.edu>
2016-03-14 14:40:02 -0500

    Verify we can read a property request and return the value of the property.

--- auralink/websocket.py
+++ auralink/websocket.py
@@ -4,6 +4,8 @@ import tornado
 import tornado.httpserver
 import tornado.websocket
 
+from props import root, getNode
+
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
@@ -11,9 +13,17 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         print 'message received:  %s' % message
         # Reverse Message and send it back
-        print 'sending back message: %s' % message[::-1]
-        self.write_message(message[::-1])
- 
+        #print 'sending back message: %s' % message[::-1]
+        #self.write_message(message[::-1])
+        tmp = message.split('/')
+        tmppath = '/'.join(tmp[0:-1])
+        if tmppath == '':
+            tmppath = '/'
+        node = getNode(tmppath, True)
+        name = tmp[-1]
+        value = node.getString(name)
+        self.write_message(message + ': ' + value)
+        
     def on_close(self):
         print 'connection closed'
  
