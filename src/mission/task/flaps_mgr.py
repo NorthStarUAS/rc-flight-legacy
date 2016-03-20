bbbbc1665ade33729275a5dfe69c2861ee0947a7
Curtis L. Olson <curtolson@flightgear.org>
2016-03-20 13:26:37 -0500

    Initial revision of a flaps manager task.  Initially just slowly moves the flap postion to the assigned setpoint.

---
+++ src/mission/task/flaps_mgr.py
@@ -0,0 +1,43 @@
+from props import root, getNode
+
+import comms.events
+from task import Task
+
+class FlapsMgr(Task):
+    def __init__(self, config_node):
+        Task.__init__(self)
+        self.flight_node = getNode("/controls/flight", True)
+        self.imu_node = getNode("/sensors/imu", True)
+        
+        self.name = config_node.getString("name")
+        self.nickname = config_node.getString("nickname")
+        self.last_time = 0.0
+        self.speed_secs = 5     # full range of motion over 5 seconds
+
+    def activate(self):
+        self.active = True
+        comms.events.log("flap_mgr", "active")
+        self.last_time = self.imu_node.getFloat("timestamp")
+    
+    def update(self):
+        if not self.active:
+            return False
+
+        cur_time = self.imu_node.getFloat("timestamp")
+        dt = cur_time - self.last_Time
+        self.last_time = cur_time
+        target_value = self.flight_node.getFloat("flaps_setpoint")
+        current_value = self.flight_node.getFloat("flaps")
+        df = target_value - current_value
+        max = dt / self.speed_secs
+        if df > max: df = max
+        if df < -max: df = -max
+        current_value += df
+        self.flight_node.setFloat("flaps", current_value)
+
+    def is_complete(self):
+        return False
+    
+    def close(self):
+        self.active = False
+        return True
