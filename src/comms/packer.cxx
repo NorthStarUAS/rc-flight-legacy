0a0aef8e8509b044d695a79c62b61056d497da00
Curtis L. Olson <olson126@umn.edu>
2016-04-13 16:34:53 -0500

    Begin stubbing in C++ interface to the python packer.

---
+++ src/comms/packer.cxx
@@ -0,0 +1,69 @@
+#include "packer.hxx"
+
+pyModulePacker::pyModulePacker()
+{
+}
+
+bool pyModulePacker::open(const char *path)
+{
+    if (pModuleObj == NULL) {
+	printf("ERROR: events.init() failed\n");
+	return false;
+    }
+    PyObject *pFuncOpen = PyObject_GetAttrString(pModuleObj, "open");
+    if ( pFuncOpen == NULL || ! PyCallable_Check(pFuncOpen) ) {
+	if ( PyErr_Occurred() ) PyErr_Print();
+	printf("ERROR: cannot find function 'update()'\n");
+	return false;
+    }
+
+    // FIXME decref pFuncOpen
+    
+    PyObject *pValue = PyObject_CallFunction(pFuncOpen, (char *)"s", path);
+    if (pValue != NULL) {
+	bool result = PyObject_IsTrue(pValue);
+	Py_DECREF(pValue);
+	return result;
+    } else {
+	PyErr_Print();
+	printf("ERROR: call failed\n");
+	return false;
+    }
+    return false;
+}
+
+#if 0
+bool pyModulePacker::log(const char *header, const char *message)
+{
+    if (pModuleObj == NULL) {
+	printf("ERROR: events.init() failed\n");
+	return false;
+    }
+    PyObject *pFuncLog = PyObject_GetAttrString(pModuleObj, "log");
+    if ( pFuncLog == NULL || ! PyCallable_Check(pFuncLog) ) {
+	if ( PyErr_Occurred() ) PyErr_Print();
+	printf("ERROR: cannot find function 'log()'\n");
+	return false;
+    }
+
+    // FIXME decref pFuncLog
+    
+    PyObject *pValue = PyObject_CallFunction(pFuncLog, (char *)"ss", header, message);
+    if (pValue != NULL) {
+	bool result = PyObject_IsTrue(pValue);
+	Py_DECREF(pValue);
+	return result;
+    } else {
+	PyErr_Print();
+	printf("ERROR: call failed\n");
+	return false;
+    }
+    return false;
+}
+#endif
+
+// FIXME: close log file!
+bool pyModulePacker::close()
+{
+    return true;
+}
---
+++ src/comms/packer.hxx
@@ -0,0 +1,24 @@
+#ifndef _AURA_PACKER_HXX
+#define _AURA_PACKER_HXX
+
+// import a python module and call it's init() and update() routines
+// requires imported python modules to follow some basic rules to play
+// nice.  (see examples in the code for now.)
+
+#include "python/pymodule.hxx"
+
+class pyModulePacker: public pyModuleBase {
+
+public:
+
+    // constructor / destructor
+    pyModulePacker();
+    ~pyModulePacker() {}
+
+    bool open(const char *path);
+    // bool log(const char *header, const char *message);
+    bool close();
+    
+};
+
+#endif // _AURA_PACKER_HXX
