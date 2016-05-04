067892eafc15492ab0e9a397a1b1477cba1007ca
Curtis L. Olson <curtolson@flightgear.org>
2016-05-04 08:36:06 -0500

    Generate gps text record as a prototype for exporting flight logs in text format.

--- auralink/auraexport.py
+++ auralink/auraexport.py
@@ -10,11 +10,31 @@ import commands
 import current
 import parser
 
+def generate_record(id, index):
+    if id == parser.GPS_PACKET_V1 or id == parser.GPS_PACKET_V2:
+        gps_node = getNode('/sensors/gps[%d]' % index, True)
+        record = "%.3f\t%.10f\t%.10f\t%.2f\t%.4f\t%.4f\t%.4f\t%f\t%d\t%d\n" % \
+		 (gps_node.getFloat('timestamp'),
+		  gps_node.getFloat('latitude_deg'),
+                  gps_node.getFloat('longitude_deg'),
+                  gps_node.getFloat('altitude_m'),
+		  gps_node.getFloat('vn_ms'),
+                  gps_node.getFloat('ve_ms'),
+                  gps_node.getFloat('vd_ms'),
+		  gps_node.getFloat('unix_time_sec'),
+                  gps_node.getInt('satellites'),
+                  gps_node.getInt('status')
+                 )
+        return record
+
+
 argparser = argparse.ArgumentParser(description='aura export')
 argparser.add_argument('--flight', help='load specified flight log')
 argparser.add_argument('--skip-seconds', help='seconds to skip when processing flight log')
 
 args = argparser.parse_args()
+
+data = {}
 
 if args.flight:
     filename = args.flight
@@ -36,14 +56,20 @@ if args.flight:
     print "len of decompressed file:", len(full)
 
     while True:
-        try:
+        #try:
             (id, index) = parser.file_read(full)
-            if id == parser.GPS_PACKET_V2:
-                print "gps packet"
-            if id == parser.FILTER_PACKET_V2:
-                print "filter packet"
-        except:
-            print "end of file"
-            break
+            key = '%d-%d' % (id, index)
+            record = generate_record(id, index)
+            if key in data:
+                data[key] += 1
+            else:
+                data[key] = 1
+        #except:
+        #    print "end of file"
+        #    break
 else:
     print "A flight log file must be provided"
+
+for key in sorted(data):
+    print key, ":", data[key]
+
