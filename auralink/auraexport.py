9a1d683d0feb318702caf4f8eecf2411a36a37ee
Curtis L. Olson <olson126@umn.edu>
2016-05-05 11:42:38 -0500

    Step towards categorizing/naming data for output files.

--- auralink/auraexport.py
+++ auralink/auraexport.py
@@ -14,33 +14,63 @@ import commands
 import current
 import parser
 
-
-def generate_record(id, index, delim=','):
+def logical_category(id):
     if id == parser.GPS_PACKET_V1 or id == parser.GPS_PACKET_V2:
+        return 'gps'
+    elif id == parser.IMU_PACKET_V1 or id == parser.IMU_PACKET_V2 \
+         or id == parser.IMU_PACKET_V3:
+        return 'imu'
+    elif id == parser.AIRDATA_PACKET_V3 or id == parser.AIRDATA_PACKET_V4 \
+         or id == parser.AIRDATA_PACKET_V5:
+        return 'air'
+    elif id == parser.FILTER_PACKET_V1 or id == parser.FILTER_PACKET_V2:
+        return 'filter'
+    elif id == parser.ACTUATOR_PACKET_V1 or id == parser.ACTUATOR_PACKET_V2:
+        return 'act'
+    elif id == parser.PILOT_INPUT_PACKET_V1 \
+         or id == parser.PILOT_INPUT_PACKET_V2:
+        return 'pilot'
+    elif id == parser.AP_STATUS_PACKET_V1 or id == parser.AP_STATUS_PACKET_V2 \
+         or id == parser.AP_STATUS_PACKET_V3:
+        return 'ap'
+    elif id == parser.SYSTEM_HEALTH_PACKET_V2 \
+         or id == parser.SYSTEM_HEALTH_PACKET_V3 \
+         or id == parser.SYSTEM_HEALTH_PACKET_V4:
+        return 'health'
+    elif id == parser.PAYLOAD_PACKET_V1 or id == parser.PAYLOAD_PACKET_V2:
+        return 'payload'
+
+# When a binary record of some id is read, it gets parsed into the
+# property tree structure.  The following code simple calls the
+# appropriate text packer function for the given id to extract the
+# same data back out of the property tree and format it as a text
+# record.
+def generate_record(category, index, delim=','):
+    if category == 'gps':
         record = comms.packer.pack_gps_text(index, delim)
         return record
-    elif id == parser.IMU_PACKET_V1 or id == parser.IMU_PACKET_V2 or id == parser.IMU_PACKET_V3:
+    elif category == 'imu':
         record = comms.packer.pack_imu_text(index, delim)
         return record
-    elif id == parser.AIRDATA_PACKET_V3 or id == parser.AIRDATA_PACKET_V4 or id == parser.AIRDATA_PACKET_V5:
+    elif category == 'air':
         record = comms.packer.pack_airdata_text(index, delim)
         return record
-    elif id == parser.FILTER_PACKET_V1 or id == parser.FILTER_PACKET_V2:
+    elif category == 'filter':
         record = comms.packer.pack_filter_text(index, delim)
         return record
-    elif id == parser.ACTUATOR_PACKET_V1 or id == parser.ACTUATOR_PACKET_V2:
+    elif category == 'act':
         record = comms.packer.pack_act_text(index, delim)
         return record
-    elif id == parser.PILOT_INPUT_PACKET_V1 or id == parser.PILOT_INPUT_PACKET_V2:
+    elif category == 'pilot':
         record = comms.packer.pack_pilot_text(index, delim)
         return record
-    elif id == parser.AP_STATUS_PACKET_V1 or id == parser.AP_STATUS_PACKET_V2 or id == parser.AP_STATUS_PACKET_V3:
+    elif category == 'ap':
         record = comms.packer.pack_ap_status_text(index, delim)
         return record
-    elif id == parser.SYSTEM_HEALTH_PACKET_V2 or id == parser.SYSTEM_HEALTH_PACKET_V3 or id == parser.SYSTEM_HEALTH_PACKET_V4:
+    elif category == 'health':
         record = comms.packer.pack_system_health_text(index, delim)
         return record
-    elif id == parser.PAYLOAD_PACKET_V1 or id == parser.PAYLOAD_PACKET_V2:
+    elif category == 'payload':
         record = comms.packer.pack_payload_text(index, delim)
         return record
     # remote_link_node.setInt('sequence_num', result[14]) # include in system health....
@@ -76,8 +106,9 @@ if args.flight:
     while True:
         try:
             (id, index) = parser.file_read(full)
-            key = '%d-%d' % (id, index)
-            record = generate_record(id, index)
+            category = logical_category(id)
+            record = generate_record(category, index)
+            key = '%s-%d' % (category, index)
             if key in data:
                 data[key].append(record)
             else:
