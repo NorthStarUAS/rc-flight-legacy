f73cb44ceb73e32d967ee254e1a101b29f3d19e1
Curtis L. Olson <curtolson@flightgear.org>
2016-05-22 13:11:42 -0500

    Reorg and update autostart scripts a bit.

--- scripts/systemd/bb-aura.sh
+++ scripts/systemd/bb-aura.sh
@@ -7,4 +7,4 @@ HOME=~root
 echo "starting aura autopilot in $HOME"
 cd $HOME
 pwd
-/usr/local/bin/aura --python_path $HOME/Source/aura-core/src --remote-link on > /var/log/aura.log
+/root/bin/aura --python_path $HOME/src/aura-core/src --remote-link on > /var/log/aura.log
