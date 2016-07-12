371a91de11bfbacc3a1b6fbd6038df6b0ef9f3b2
Curtis L. Olson <olson126@umn.edu>
2016-07-12 13:45:33 -0500

    Updates for Tyr controls output mapping.

--- src/actuators/act_goldy2.cxx
+++ src/actuators/act_goldy2.cxx
@@ -26,6 +26,7 @@ static netSocket sock;
 static int port = 0;
 static string hostname = "";
 static string output_mode = "radians";
+static string controls_mapping = "vireo";
 
 // property nodes
 static pyPropertyNode act_node;
@@ -40,6 +41,9 @@ static void bind_input( pyPropertyNode *config ) {
     }
     if ( config->hasChild("port") ) {
 	port = config->getLong("port");
+    }
+    if ( config->hasChild("controls_mapping") ) {
+	controls_mapping = config->getString("controls_mapping");
     }
     if ( config->hasChild("output_mode") ) {
 	output_mode = config->getString("output_mode");
@@ -122,6 +126,7 @@ bool goldy2_act_update() {
     // static uint16_t pos = 1000;
     const int goldy2_act_size = 68;
     const int packet_size = 60;	// 6*n, n=10
+    const int max_act = 10;
     uint8_t packet_buf[goldy2_act_size];
     uint8_t *start_buf = packet_buf;
     uint8_t *buf = packet_buf;
@@ -136,47 +141,94 @@ bool goldy2_act_update() {
     double aileron = act_node.getDouble("channel", 0);
     double elevator = act_node.getDouble("channel", 1);
     double throttle = act_node.getDouble("channel", 2);
+    double rudder = act_node.getDouble("channel", 3);
+    // double gear = act_node.getDouble("channel", 4);
+    double flaps = act_node.getDouble("channel", 5);
     
     // printf("ail=%.2f ele=%.2f\n", aileron, elevator);
 
     int thr_pwm = gen_pulse( throttle, false );
-    int left_act = 0;
-    int right_act = 0;
     int zero_act = 0;
     int units = 0; // 0 = PWM, 1 = angle control (radians)
     
     if ( output_mode == "pwm" ) {
-	double left_cmd = elevator*0.5 - aileron*0.5;
-	double right_cmd = -elevator*0.5 - aileron*0.5;
-	left_act = gen_pulse( left_cmd, true );
-	right_act = gen_pulse( right_cmd, true );
 	zero_act = PWM_CENTER;
 	units = 0;
     } else if ( output_mode == "radians" ) {
-	left_act = (elevator - aileron) * 1000.0;
-	if ( left_act < -1570 ) { left_act = -1570; }
-	if ( left_act > 1570 ) { left_act = 1570; }
-	right_act = (elevator + aileron) * 1000.0;
-	if ( right_act < -1570 ) { right_act = -1570; }
-	if ( right_act > 1570 ) { right_act = 1570; }
 	zero_act = 0.0;
 	units = 1;
     }
+    int act_out[max_act] = { zero_act };
+
+    if ( controls_mapping == "vireo" ) {
+	int right_act = 0;
+	int left_act = 0;
+	if ( output_mode == "pwm" ) {
+	    double right_cmd = -elevator*0.5 - aileron*0.5;
+	    double left_cmd = elevator*0.5 - aileron*0.5;
+	    right_act = gen_pulse( right_cmd, true );
+	    left_act = gen_pulse( left_cmd, true );
+	} else if ( output_mode == "radians" ) {
+	    right_act = (elevator + aileron) * 1000.0;
+	    if ( right_act < -1570 ) { right_act = -1570; }
+	    if ( right_act > 1570 ) { right_act = 1570; }
+	    left_act = (elevator - aileron) * 1000.0;
+	    if ( left_act < -1570 ) { left_act = -1570; }
+	    if ( left_act > 1570 ) { left_act = 1570; }
+	}
+	act_out[2] = thr_pwm;
+	act_out[3] = right_act;
+	act_out[4] = left_act;
+    } else if ( controls_mapping == "tyr" ) {
+	int ele_act = 0;
+	int rud_act = 0;
+	int right_ail_act = 0;
+	int left_ail_act = 0;
+	int right_flap_act = 0;
+	int left_flap_act = 0;
+	if ( output_mode == "pwm" ) {
+	    ele_act = gen_pulse( elevator, true );
+	    rud_act = gen_pulse( rudder, true );
+	    right_ail_act = gen_pulse( aileron, true );
+	    left_ail_act = gen_pulse( -aileron, true );
+	    right_flap_act = gen_pulse( flaps, true );
+	    left_flap_act = gen_pulse( flaps, true );
+	} else if ( output_mode == "radians" ) {
+	    ele_act = elevator * 1000.0;
+	    if ( ele_act < -1570 ) { ele_act = -1570; }
+	    if ( ele_act > 1570 ) { ele_act = 1570; }
+	    rud_act = rudder * 1000.0;
+	    if ( rud_act < -1570 ) { rud_act = -1570; }
+	    if ( rud_act > 1570 ) { rud_act = 1570; }
+	    right_ail_act = aileron * 1000.0;
+	    if ( right_ail_act < -1570 ) { right_ail_act = -1570; }
+	    if ( right_ail_act > 1570 ) { right_ail_act = 1570; }
+	    left_ail_act = -aileron * 1000.0;
+	    if ( left_ail_act < -1570 ) { left_ail_act = -1570; }
+	    if ( left_ail_act > 1570 ) { left_ail_act = 1570; }
+	    right_flap_act = flaps * 1000.0;
+	    if ( right_flap_act < -1570 ) { right_flap_act = -1570; }
+	    if ( right_flap_act > 1570 ) { right_flap_act = 1570; }
+	    left_flap_act = flaps * 1000.0;
+	    if ( left_flap_act < -1570 ) { left_flap_act = -1570; }
+	    if ( left_flap_act > 1570 ) { left_flap_act = 1570; }
+	}
+	act_out[2] = thr_pwm;
+	act_out[3] = rud_act;
+	act_out[4] = ele_act;
+	act_out[5] = left_ail_act;
+	act_out[6] = left_flap_act;
+	act_out[7] = right_flap_act;
+	act_out[8] = right_ail_act;
+    }
     
-    int val;
     for ( uint8_t i = 0; i < 10; i++ ) {
 	int ch_units = units;
+	int val = act_out[i];
 	if ( i == 2 ) {
-            val = thr_pwm;
+            // channel 2 is always throttle, always pwm units
 	    ch_units = 0;
-        } else if ( i == 3 ) {
-	    val = right_act;
-	} else if ( i == 4 ) {
-	    val = left_act;
-	} else {
-	    val = zero_act;
 	}
-        // val = pos;
 	*(uint8_t *)buf = i; buf++;
 	*(uint8_t *)buf = ch_units; buf++;
         if ( ch_units == 0 ) {
@@ -188,7 +240,6 @@ bool goldy2_act_update() {
         }
     }
 
- 
     uint16_t CRC = utilCRC16(start_buf+3, packet_size+3, 0);
     uint16_t CRC_msb = CRC / 256;
     uint16_t CRC_lsb = CRC - (CRC_msb * 256);
