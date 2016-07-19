f342787aff1381c612dfe2c32557b23ede111dea
Curtis L. Olson <olson126@umn.edu>
2016-07-19 13:30:14 -0500

    Work through edits to make script work again without the original (slightly less correct per-axis linear fit.)

--- scripts/NavPy/magcal_ekf.py
+++ scripts/NavPy/magcal_ekf.py
@@ -48,8 +48,8 @@ if args.flight:
         time, lat, lon, alt, vn, ve, vd, phi, the, psi, status = re.split('[,\s]+', line.rstrip())
         filter_data.append( [time, lat, lon, alt, vn, ve, vd, phi, the, psi] )
     if not len(filter_data):
-        print "No imu records loaded, cannot continue..."
-        sys.exit()
+        print "No filter records loaded, cannot continue..."
+        quit()
 elif args.sentera:
     imu_file = os.path.join(args.sentera, "imu.csv")
     filter_file = os.path.join(args.sentera, "filter-post.txt")
@@ -84,15 +84,14 @@ elif args.sentera:
         time, lat, lon, alt, vn, ve, vd, phi, the, psi, status = re.split('[,\s]+', line.rstrip())
         filter_data.append( [time, lat, lon, alt, vn, ve, vd, phi, the, psi] )
     if not len(filter_data):
-        print "No imu records loaded, cannot continue..."
+        print "No filter records loaded, cannot continue..."
         quit()
      
 imu_data = np.array(imu_data, dtype=np.float64)
 x = imu_data[:,0]
-print 'imu_data:', imu_data[:,7]
-print 'hx range:', imu_data[:,7].min(), imu_data[:,7].max()
-print 'hy range:', imu_data[:,8].min(), imu_data[:,8].max()
-print 'hz range:', imu_data[:,9].min(), imu_data[:,9].max()
+#print 'hx range:', imu_data[:,7].min(), imu_data[:,7].max()
+#print 'hy range:', imu_data[:,8].min(), imu_data[:,8].max()
+#print 'hz range:', imu_data[:,9].min(), imu_data[:,9].max()
 imu_hx = interpolate.interp1d(x, imu_data[:,7], bounds_error=False, fill_value=0.0)
 imu_hy = interpolate.interp1d(x, imu_data[:,8], bounds_error=False, fill_value=0.0)
 imu_hz = interpolate.interp1d(x, imu_data[:,9], bounds_error=False, fill_value=0.0)
@@ -115,9 +114,11 @@ print "starting at:", lat, lon
 gm = geomag.geomag.GeoMag("/usr/lib/python2.7/site-packages/geomag/WMM.COF")
 mag = gm.GeoMag(base_lat, base_lon)
 mag_ned = np.array( [mag.bx, mag.by, mag.bz] )
+print 'raw mag vector:', mag_ned
 norm = np.linalg.norm(mag_ned)
+print '          norm:', norm
 mag_ned /= norm
-print mag_ned
+print '    normalized:', mag_ned
 
 # read the events.txt file to determine when aircraft becomes airborne
 # (so we can ignore preflight values.)  Update: also to read the IMU
@@ -188,7 +189,7 @@ for i, x in enumerate( np.linspace(xmin, xmax, trange*args.resample_hz) ):
     N2B = navpy.angle2dcm(psi, the, phi, input_unit='deg')
     mag_ideal = N2B.dot(mag_ned)
     norm = np.linalg.norm(mag_ideal)
-    #mag_ideal /= norm
+    mag_ideal /= norm
     hx = imu_hx(x)
     hy = imu_hy(x)
     hz = imu_hz(x)
@@ -196,8 +197,6 @@ for i, x in enumerate( np.linspace(xmin, xmax, trange*args.resample_hz) ):
         print "oops:", hx, hy, hz
     mag_sense = np.array([hx, hy, hz])
     #print mag_sense
-    #norm = np.linalg.norm(mag_sense)
-    #ag_sense /= norm
     if args.flight:
         ideal_data.append( mag_ideal[:].tolist() )
         sense_data.append( mag_sense[:].tolist() )
@@ -205,10 +204,12 @@ for i, x in enumerate( np.linspace(xmin, xmax, trange*args.resample_hz) ):
         ideal_data.append( mag_ideal[:].tolist() )
         sense_data.append( mag_sense[:].tolist() )
 
-    #print mag_ideal[0], mag_ideal[1], mag_ideal[2], mag_sense[0], mag_sense[1], mag_sense[2]
-    
 ideal_array = np.array(ideal_data, dtype=np.float64)
 sense_array = np.array(sense_data, dtype=np.float64)
+
+# compute affine transformation between sensed data and ideal data,
+# this is our best estimate of the ideal magnetometer calibration
+# using the EKF inertial only solution.
 
 affine = transformations.affine_matrix_from_points(sense_array.T, ideal_array.T)
 print "affine:"
@@ -220,7 +221,7 @@ print ' angles:', angles
 print ' trans:', translate
 print ' persp:', perspective
 
-# write calibration data to file (so we can aggregate over
+# write calibration data points to file (so we can aggregate over
 # multiple flights later
 if args.flight:
     data_dir = os.path.abspath(args.flight)
@@ -241,115 +242,74 @@ for i in range(ideal_array.shape[0]):
 f.close()
 
     
-def gen_func( coeffs, min, max, steps ):
-    print min, max, steps
-    step = (max - min) / steps
-    xvals = []
-    yvals = []
-    func = np.poly1d(coeffs)
-    for x in np.arange(min, max+step, step):
-        y = func(x)
-        xvals.append(x)
-        yvals.append(y)
-    return xvals, yvals
+# def gen_func( coeffs, min, max, steps ):
+#     print min, max, steps
+#     step = (max - min) / steps
+#     xvals = []
+#     yvals = []
+#     func = np.poly1d(coeffs)
+#     for x in np.arange(min, max+step, step):
+#         y = func(x)
+#         xvals.append(x)
+#         yvals.append(y)
+#     return xvals, yvals
 
-deg = 1
-hx_fit, res, _, _, _ = np.polyfit( sense_array[:,0], ideal_array[:,0], deg, full=True )
-hy_fit, res, _, _, _ = np.polyfit( sense_array[:,1], ideal_array[:,1], deg, full=True )
-hz_fit, res, _, _, _ = np.polyfit( sense_array[:,2], ideal_array[:,2], deg, full=True )
+# individual axis linear fitting
+#deg = 1
+#hx_fit, res, _, _, _ = np.polyfit( sense_array[:,0], ideal_array[:,0], deg, full=True )
+#hy_fit, res, _, _, _ = np.polyfit( sense_array[:,1], ideal_array[:,1], deg, full=True )
+#hz_fit, res, _, _, _ = np.polyfit( sense_array[:,2], ideal_array[:,2], deg, full=True )
 
-hx_fit_inv, res, _, _, _ = np.polyfit( ideal_array[:,0], sense_array[:,0], deg, full=True )
+#hx_fit_inv, res, _, _, _ = np.polyfit( ideal_array[:,0], sense_array[:,0], deg, full=True )
 
-print hx_fit, hy_fit, hz_fit
-
-ellipse_fit = False
-if ellipse_fit:
-    # test
-    import mag
-    m = mag.Magnetometer(F=1.0)
-    m.calibrate_bulk(sense_array)
-    print "b:", m.b
-    print "A_1:", m.A_1
-
-    hx_curt = np.poly1d(hx_fit)
-    hy_curt = np.poly1d(hy_fit)
-    hz_curt = np.poly1d(hz_fit)
-    curt_data = []
-    ef_data = []
-    for s in sense_data:
-        cfx = hx_curt(s[0])
-        cfy = hy_curt(s[1])
-        cfz = hz_curt(s[2])
-        curt = np.array( [cfx, cfy, cfz] )
-        norm = np.linalg.norm(curt)
-        #curt /= norm
-        curt_data.append(curt)
-
-        ef = m.map(s)
-        norm = np.linalg.norm(ef)
-        #ef /= norm
-        ef_data.append(ef)
-        #print 's:', s, 'cf:', curt, 'ef:', ef
-    curt_array = np.array(curt_data)
-    ef_array = np.array(ef_data)
-
-    affine = transformations.affine_matrix_from_points(sense_array.T, ef_array.T)
-    print "affine ef:"
-    print affine
-    scale, shear, angles, translate, perspective = transformations.decompose_matrix(affine)
-    print ' scale:', scale
-    print ' shear:', shear
-    print ' angles:', angles
-    print ' trans:', translate
-    print ' persp:', perspective
+#print hx_fit, hy_fit, hz_fit
 
 # generate affine mapping
 af_data = []
-for s in sense_array:
+for i, s in enumerate(sense_array):
     hs = np.hstack( [s, 1.0] )
     af = np.dot(affine, hs)
-    norm = np.linalg.norm(af)
-    #af /= norm
-    af_data.append(af)
-    #print 's:', s, 'af:', af
+    norm = np.linalg.norm(af[:3])
+    #print 'affine mapped vector norm:', norm
+    #print 'ideal:', ideal_array[i], 'af:', af[:3]
+    af[:3] /= norm
+    af_data.append(af[:3])
 af_array = np.array(af_data)
 
 
 if args.plot:
     cal_fig, cal_mag = plt.subplots(3, sharex=True)
     cal_mag[0].plot(sense_array[:,0],ideal_array[:,0],'r.',alpha=0.5,label='EKF Estimate')
-    cal_mag[0].plot(sense_array[:,0],ef_array[:,0],'b.',alpha=0.5,label='Ellipsoid Cal')
     cal_mag[0].plot(sense_array[:,0],af_array[:,0],'g.',alpha=0.5,label='Affine Cal')
-    xvals, yvals = gen_func(hx_fit, sense_array[:,0].min(), sense_array[:,0].max(), 100)
-    cal_mag[0].plot(xvals,yvals,'y',label='EKF Fit')
+    #xvals, yvals = gen_func(hx_fit, sense_array[:,0].min(), sense_array[:,0].max(), 100)
+    #cal_mag[0].plot(xvals,yvals,'y',label='EKF Fit')
     cal_mag[0].set_xlabel('(hx) Sensed Mag')
     cal_mag[0].set_ylabel('(hx) Ideal Mag Est')
     cal_mag[0].set_title('Magnetometer Calibration')
     cal_mag[0].legend(loc=0)
 
     cal_mag[1].plot(sense_array[:,1],ideal_array[:,1],'r.',alpha=0.5,label='hy')
-    cal_mag[1].plot(sense_array[:,1],ef_array[:,1],'b.',alpha=0.5,label='hy')
     cal_mag[1].plot(sense_array[:,1],af_array[:,1],'g.',alpha=0.5,label='hy')
-    xvals, yvals = gen_func(hy_fit, sense_array[:,1].min(), sense_array[:,1].max(), 100)
-    cal_mag[1].plot(xvals,yvals,'y',label='hx')
+    #xvals, yvals = gen_func(hy_fit, sense_array[:,1].min(), sense_array[:,1].max(), 100)
+    #cal_mag[1].plot(xvals,yvals,'y',label='hx')
     cal_mag[1].set_xlabel('(hy) Sensed Mag')
     cal_mag[1].set_ylabel('(hy) Ideal Mag Est')
 
     cal_mag[2].plot(sense_array[:,2],ideal_array[:,2],'r.',alpha=0.5,label='hz')
-    cal_mag[2].plot(sense_array[:,2],ef_array[:,2],'b.',alpha=0.5,label='hz')
     cal_mag[2].plot(sense_array[:,2],af_array[:,2],'g.',alpha=0.5,label='hz')
-    xvals, yvals = gen_func(hz_fit, sense_array[:,2].min(), sense_array[:,2].max(), 100)
-    cal_mag[2].plot(xvals,yvals,'y',label='hz')
+    #xvals, yvals = gen_func(hz_fit, sense_array[:,2].min(), sense_array[:,2].max(), 100)
+    #cal_mag[2].plot(xvals,yvals,'y',label='hz')
     cal_mag[2].set_xlabel('(hz) Sensed Mag')
     cal_mag[2].set_ylabel('(hz) Ideal Mag')
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     #ax.scatter(sense_array[:,0], sense_array[:,1], sense_array[:,2])
-    ax.scatter(curt_array[:,0], curt_array[:,1], curt_array[:,2], c='b',alpha=0.5)
-    ax.scatter(af_array[:,0], af_array[:,1], af_array[:,2], c='r',alpha=0.5)
+    ax.scatter(ideal_array[:,0], ideal_array[:,1], ideal_array[:,2], c='b',alpha=0.5,label='Ideal Mag (EKF)')
+    ax.scatter(af_array[:,0], af_array[:,1], af_array[:,2], c='r',alpha=0.5,label='Calibrated Mag')
     ax.set_xlabel('hx')
     ax.set_ylabel('hy')
     ax.set_zlabel('hz')
+    ax.legend(loc=0)
     plt.show()
 
