63134149797f9f4be30b2f15893fd16f2ce830df
Curtis L. Olson <olson126@umn.edu>
2016-08-09 11:43:31 -0500

    Request nice level of -10 when running (should be higher priority than everything else in the system.)

--- scripts/systemd/bb-aura.sh
+++ scripts/systemd/bb-aura.sh
@@ -7,4 +7,4 @@ HOME=~root
 echo "starting aura autopilot in $HOME"
 cd $HOME
 pwd
-/root/bin/aura --python_path $HOME/src/aura-core/src --remote-link on > /var/log/aura.log
+nice -n -10 /root/bin/aura --python_path $HOME/src/aura-core/src --remote-link on > /var/log/aura.log
