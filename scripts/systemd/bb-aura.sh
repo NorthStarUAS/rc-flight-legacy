09da7a569446c684c12de817dc3da4cf75a445ce
Curtis L. Olson <olson126@umn.edu>
2016-08-09 12:00:09 -0500

    Parameterize nice value

--- scripts/systemd/bb-aura.sh
+++ scripts/systemd/bb-aura.sh
@@ -3,8 +3,12 @@
 # set fastest cpu performance possible
 cpufreq-set -g performance
 
+# nice range is -20 to 20 with -20 being the highest priority.  -10 should be
+# a good balanced number.
+NICE=-10
+
 HOME=~root
+
 echo "starting aura autopilot in $HOME"
 cd $HOME
-pwd
-nice -n -10 /root/bin/aura --python_path $HOME/src/aura-core/src --remote-link on > /var/log/aura.log
+nice -n $NICE $HOME/bin/aura --python_path $HOME/src/aura-core/src --remote-link on > /var/log/aura.log
