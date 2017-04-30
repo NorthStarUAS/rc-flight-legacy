6cb62a22f67c90af86f1609bf3e5ced350ee6b5c
Curtis L. Olson <curtolson@flightgear.org>
2017-04-30 09:19:17 -0500

    Pointless grouping and reordering.

--- src/comms/packet_id.py
+++ src/comms/packet_id.py
@@ -1,33 +1,46 @@
 # shared packet id definitions
 
-# note: it is best to add new packet id's instead of changing or
-# reusing numbers to maximize backwards compatibility with older
-# binary log files.
+# note: this id is encoded as a single byte in the binary packet
+# format so the max id number we can assign is 255.  As long as we
+# have numbers available, it is best to add new packet id's instead of
+# changing or reusing numbers to maximize backwards compatibility with
+# older binary log files.
 
 GPS_PACKET_V1 = 0
-IMU_PACKET_V1 = 1
-FILTER_PACKET_V1 = 2
-ACTUATOR_PACKET_V1 = 3
-PILOT_INPUT_PACKET_V1 = 4
-AP_STATUS_PACKET_V1 = 5
-AIRDATA_PACKET_V3 = 9
-AP_STATUS_PACKET_V2 = 10
-SYSTEM_HEALTH_PACKET_V2 = 11
-PAYLOAD_PACKET_V1 = 12
-AIRDATA_PACKET_V4 = 13
-SYSTEM_HEALTH_PACKET_V3 = 14
-IMU_PACKET_V2 = 15
 GPS_PACKET_V2 = 16
-IMU_PACKET_V3 = 17
-AIRDATA_PACKET_V5 = 18
-SYSTEM_HEALTH_PACKET_V4 = 19
-PILOT_INPUT_PACKET_V2 = 20
-ACTUATOR_PACKET_V2 = 21
-FILTER_PACKET_V2 = 22
-PAYLOAD_PACKET_V2 = 23
-AP_STATUS_PACKET_V3 = 24
-RAVEN_PACKET_V1 = 25
 GPS_PACKET_V3 = 26
+
+IMU_PACKET_V1 = 1
+IMU_PACKET_V2 = 15
+IMU_PACKET_V3 = 17
+
+FILTER_PACKET_V1 = 2
+FILTER_PACKET_V2 = 22
+
+ACTUATOR_PACKET_V1 = 3
+ACTUATOR_PACKET_V2 = 21
+
+PILOT_INPUT_PACKET_V1 = 4
+PILOT_INPUT_PACKET_V2 = 20
+
+AP_STATUS_PACKET_V1 = 5
+AP_STATUS_PACKET_V2 = 10
+AP_STATUS_PACKET_V3 = 24
+
+AIRDATA_PACKET_V3 = 9
+AIRDATA_PACKET_V4 = 13
+AIRDATA_PACKET_V5 = 18
+
+SYSTEM_HEALTH_PACKET_V2 = 11
+SYSTEM_HEALTH_PACKET_V3 = 14
+SYSTEM_HEALTH_PACKET_V4 = 19
+
+PAYLOAD_PACKET_V1 = 12
+PAYLOAD_PACKET_V2 = 23
+
 EVENT_PACKET_V1 = 27
+
 COMMAND_PACKET_V1 = 28
-REMOTE_JOYSTICK_V1 = 29
+
+RAVEN_PACKET_V1 = 25
+REMOTE_JOYSTICK_V1 = 29         # last id assigned
