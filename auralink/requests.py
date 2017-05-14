3e9f6c6468fb6ef63bd0c2e95dd78b49e526e6a3
Curtis L. Olson <curtolson@flightgear.org>
2017-05-14 07:48:11 -0500

    Add a mechanism to request specific property values from the remote side and then retry until an answer is received.

---
+++ auralink/requests.py
@@ -0,0 +1,19 @@
+# generate requests for information we want but don't yet have
+
+from props import root, getNode
+
+import commands
+
+ident_node = getNode('/config/identity', True)
+
+requests_pending = True
+def gen_requests():
+    global requests_pending
+    if not requests_pending:
+        # we have everything we asked for
+        return
+    if commands.cmd_queue_empty():
+        requests_pending = False # tentatively mark us as finished
+        if ident_node.getString('call_sign') == '':
+            requests_pending = True
+            commands.add('get,/config/identity/call_sign')
