e668946c8eb5911b7df80cc5a0bc531af0131928
Curtis L. Olson <curtolson@flightgear.org>
2017-05-14 16:02:53 -0500

    Only send "get" events over the air for now.

--- src/comms/events.py
+++ src/comms/events.py
@@ -13,9 +13,10 @@ def update():
     return True
 
 # pack and send message
-def log(header="", message=""):
+def log(header="", message="", send_to_remote=False):
     event_string = '%s: %s' % (header, message)
     buf = packer.pack_event_v1(event_string)
     logging.log_message(buf)
-    remote_link.send_message(buf)
+    if send_to_remote:
+        remote_link.send_message(buf)
     return True
