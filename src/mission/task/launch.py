70325c59a671bf1181c6fedd75c567f865825df7
Curtis L. Olson <curtolson@flightgear.org>
2017-08-14 07:43:22 -0500

    Rearrange some logic and code to make the take-off task more robust. I discovered that past versions may not switch to 'full flying' mode until the end of the task if starting elevator trim never allows the aircraft to reach threshold speed.  Now also consider the "is_airborne" flag. Also, don't zero the throttle on 'full flying' mode activation if we are already airborne (i.e. a hand launch.)

--- src/mission/task/launch.py
+++ src/mission/task/launch.py
@@ -60,19 +60,26 @@ class Launch(Task):
         if not self.active:
             return False
         
+        throttle_time_sec = 2.0 # hard code for now (fixme: move to config)
+        feather_time = 5.0      # fixme: another config option
+        
+        is_airborne = self.task_node.getBool("is_airborne")
+            
         # For wheeled take offs, track relative heading (initialized to
         # zero) when autopilot mode is engaged and steer that error to
         # zero with the rudder until flying/climbing
 
         if self.ap_node.getBool("master_switch"):
             if not self.last_ap_master:
-                # reset on entering AP mode
+                # reset states when engaging AP mode
                 self.relhdg = 0.0
                 self.control_limit = 1.0
                 self.flight_node.setFloat("flaps_setpoint", self.flaps)
+                if not is_airborne:
+                    # if engaging on the ground, start with zero throttle
+                    self.engine_node.setFloat("throttle", 0.0)
 
-            throttle_time_sec = 2.0 # hard code for now (fixme: move to config)
-            if dt > 0.0:
+            if not is_airborne:
                 # run up throttle over the specified interval
                 throttle = self.engine_node.getFloat("throttle")
                 throttle += dt / throttle_time_sec
@@ -80,68 +87,71 @@ class Launch(Task):
                     throttle = 1.0
                 self.engine_node.setFloat("throttle", throttle)
 
-                # estimate short term heading
-                self.relhdg += self.imu_node.getFloat("r_rad_sec") * r2d * dt
+            # estimate short term heading
+            self.relhdg += self.imu_node.getFloat("r_rad_sec") * r2d * dt
 
-                # I am not clamping heading to +/- 180 here.  The
-                # rational is that if we turn more than 180 beyond our
-                # starting heading we are probably majorly screwed up,
-                # but even so, let's unwind back the direction from
-                # where we came rather than doing a hard-over reversal
-                # of the rudder back to the other direction even if it
-                # would be a shorter turning direction.  Less chance
-                # of upsetting the apple cart with a massive control
-                # input change.
+            # I am not clamping heading to +/- 180 here.  The
+            # rational is that if we turn more than 180 beyond our
+            # starting heading we are probably majorly screwed up,
+            # but even so, let's unwind back the direction from
+            # where we came rather than doing a hard-over reversal
+            # of the rudder back to the other direction even if it
+            # would be a shorter turning direction.  Less chance
+            # of upsetting the apple cart with a massive control
+            # input change.
 
-                # if airborne, then slowly feather our max steer_limit
-                # to zero over the period of 2 seconds.  This avoids a
-                # hard rudder snap to zero if we are off heading, but
-                # the assumption is once we are airborne we'll prefer
-                # to keep our wings level and fly the current heading
-                # rather than fight with our rudder and add drag to
-                # get back on the heading.
+            # if airborne, then slowly feather our max steer_limit
+            # to zero over the period of 2 seconds.  This avoids a
+            # hard rudder snap to zero if we are off heading, but
+            # the assumption is once we are airborne we'll prefer
+            # to keep our wings level and fly the current heading
+            # rather than fight with our rudder and add drag to
+            # get back on the heading.
 
-                if self.task_node.getBool("is_airborne"):
-                    feather_time = 5.0 # fixme: another config option
-                    delta = ( 1.0 / feather_time ) * dt
-                    self.control_limit -= delta
-                    if self.control_limit < 0.0:
-                        self.control_limit = 0.0
+            if is_airborne and self.control_limit > 0:
+                delta = ( 1.0 / feather_time ) * dt
+                self.control_limit -= delta
+                if self.control_limit < 0.0:
+                    self.control_limit = 0.0
 
-                if self.rudder_enable:
-                    # simple proportional controller to steer against
-                    # (estimated) heading error
-                    rudder = self.relhdg * self.rudder_gain
-                    steer_limit = self.rudder_max * self.control_limit
-                    if rudder < -steer_limit:
-                        rudder = -steer_limit
-                    if rudder > steer_limit:
-                        rudder = steer_limit
-                    self.flight_node.setFloat("rudder", rudder)
+            if self.rudder_enable:
+                # simple proportional controller to steer against
+                # (estimated) heading error
+                rudder = self.relhdg * self.rudder_gain
+                steer_limit = self.rudder_max * self.control_limit
+                if rudder < -steer_limit:
+                    rudder = -steer_limit
+                if rudder > steer_limit:
+                    rudder = steer_limit
+                self.flight_node.setFloat("rudder", rudder)
 
-                roll = -self.relhdg * self.roll_gain
-                if roll < -self.roll_limit:
-                    roll = -self.roll_limit
-                if roll > self.roll_limit:
-                    roll = self.roll_limit
-                self.targets_node.setFloat("roll_deg", roll)
+            roll = -self.relhdg * self.roll_gain
+            if roll < -self.roll_limit:
+                roll = -self.roll_limit
+            if roll > self.roll_limit:
+                roll = self.roll_limit
+            self.targets_node.setFloat("roll_deg", roll)
 
-                if self.vel_node.getFloat("airspeed_kt") > self.target_speed_kt:
-                    # we've reached our flying/climbout airspeed,
-                    # switch to pitch/elevator speed hold mode
-                    self.targets_node.setFloat("pitch_deg", self.orient_node.getFloat("pitch_deg"))
-                    self.ap_node.setString("mode", "basic+alt+speed")
+            if is_airborne or \
+               (self.vel_node.getFloat("airspeed_kt") > self.target_speed_kt):
+                # we are flying or we've reached our flying/climbout
+                # airspeed: switch to normal flight mode
+                self.targets_node.setFloat("pitch_deg", self.orient_node.getFloat("pitch_deg"))
+                self.ap_node.setString("mode", "basic+alt+speed")
 
         self.last_ap_master = self.ap_node.getBool("master_switch")
 
     def is_complete(self):
         if self.pos_node.getFloat("altitude_agl_ft") >= self.complete_agl_ft:
+            # raise flaps
+            self.flight_node.setFloat("flaps_setpoint", 0.0)
+            
+            # just in case we get to the completion altitude before
+            # we've feathered out the rudder input, let's leave the
+            # rudder centered.
             if self.rudder_enable:
-                # just in case we get to the completion altitude
-                # before we've feathered out the rudder input, let's
-                # leave the rudder centered.
                 self.flight_node.setFloat("rudder", 0.0)
-                self.flight_node.setFloat("flaps_setpoint", 0.0)
+                
             return True
         return False
     
