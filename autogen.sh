53bc369d48ca1bddde2d17d31e94e6598c387c48
Curtis L. Olson <curtolson@gmail.com>
2018-02-13 11:37:25 -0600

    Build system tweaks/cleanups.

--- autogen.sh
+++ autogen.sh
@@ -18,20 +18,6 @@ if [ ! -f NEWS ]; then
     ln -s NEWS.md NEWS
 fi
 
-AUTO_MAKE_VERSION=`automake --version | head -1 | awk '{print $4}' | sed -e 's/\.\([0-9]*\).*/\1/'`
-if test $AUTO_MAKE_VERSION -lt 15; then
-    echo ""
-    echo "You need to upgrade to automake version 1.5 or greater."
-    echo "Most distributions have packages available to install or you can"
-    echo "find the source for the most recent version at"
-    echo "ftp://ftp.gnu.org/gnu/automake"
-    exit 1
-fi
-
-echo -n "automake: `automake --version | head -1 | awk '{print $4}'`"
-echo " ($AUTO_MAKE_VERSION)"
-echo ""
-
 echo "Running aclocal"
 aclocal
 
@@ -56,13 +42,6 @@ fi
 
 echo ""
 echo "======================================"
-
-if [ -f config.cache ]; then
-    echo "config.cache exists.  Removing the config.cache file will force"
-    echo "the ./configure script to rerun all it's tests rather than using"
-    echo "the previously cached values."
-    echo ""
-fi
 
 echo ""
 echo "Now you are ready to run:"
