e7a50eb74e5d147e58f02cf3627bb7c94693269a
Curtis L. Olson <curtolson@gmail.com>
2018-03-22 12:32:55 -0500

    Follow up change to keep pace with other simplification.

--- src/comms/events.py
+++ src/comms/events.py
@@ -18,7 +18,7 @@ def log(header="", message="", send_to_remote=False):
     event_string = '%s: %s' % (header, message)
     if comms_node.getBool('display_on'):
         print event_string
-    buf = packer.pack_event_v1(event_string)
+    buf = packer.pack_event_bin(event_string)
     logging.log_message(buf)
     if send_to_remote:
         remote_link.send_message(buf)
