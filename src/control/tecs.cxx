f5a268c82750e79b31a8ddc43f561993a7d9a51e
Curtis L. Olson <curtolson@flightgear.org>
2018-05-05 08:59:48 -0500

    Reorganize the code a bit to show the flow more clearly. Remove energy total weighting (which might have been interesting, but doen't make sense to do.) Clamp the max error total based on the kinetic energy difference to prevent overspeeding in a climb.  This has a side effect of easing into a climb more gentley.

--- src/control/tecs.cxx
+++ src/control/tecs.cxx
@@ -38,9 +38,9 @@ static void init_tecs() {
 
     // force default weight values if the field is empty (so we can
     // differentiate "" from 0.0
-    if ( ! tecs_config_node.hasChild("weight_tot") ) {
-        tecs_config_node.setDouble("weight_tot", 1.0);
-    }
+    // if ( ! tecs_config_node.hasChild("weight_tot") ) {
+    //     tecs_config_node.setDouble("weight_tot", 1.0);
+    // }
     if ( ! tecs_config_node.hasChild("weight_bal") ) {
         tecs_config_node.setDouble("weight_bal", 1.0);
     }
@@ -58,16 +58,6 @@ void update_tecs() {
     double mass_kg = tecs_config_node.getDouble("mass_kg");
     if ( mass_kg < 0.01 ) { mass_kg = 2.5; }
     
-    // total weight factor
-    double wt = tecs_config_node.getDouble("weight_tot");
-    if ( wt < 0.0 ) {
-        wt = 0.0;
-        tecs_config_node.setDouble("weight_tot", wt);
-    } else if ( wt > 2.0 ) {
-        wt = 2.0;
-        tecs_config_node.setDouble("weight_tot", wt);
-    }
-    
     // balance weight factor
     double wb = tecs_config_node.getDouble("weight_bal");
     if ( wb < 0.0 ) {
@@ -81,11 +71,9 @@ void update_tecs() {
     // Current energy
     double alt_m = pos_node.getDouble("altitude_agl_m");
     double vel_mps = vel_node.getDouble("airspeed_smoothed_kt") * SG_KT_TO_MPS;
-
     double energy_pot = mass_kg * g * alt_m;
     double energy_kin = 0.5 * mass_kg * vel_mps * vel_mps;
     double energy_total = energy_pot + energy_kin;
-
     tecs_node.setDouble("energy_total", energy_total);
     tecs_node.setDouble("energy_pot", energy_pot);
     tecs_node.setDouble("energy_kin", energy_kin);
@@ -96,49 +84,55 @@ void update_tecs() {
     double target_pot = mass_kg * g * target_alt_m;
     double target_kin = 0.5 * mass_kg * target_vel_mps * target_vel_mps;
     double target_total = target_pot + target_kin;
-    
     tecs_node.setDouble("target_total", target_total);
     tecs_node.setDouble("target_pot", target_pot);
     tecs_node.setDouble("target_kin", target_kin);
 
-    // Error metrics
+    // Energy error
     double error_pot = target_pot - energy_pot;
     double error_kin = target_kin - energy_kin;
-    double error_total = wt * error_pot + (2.0 - wt) * error_kin;
-
-    // Compute min & max kinetic energy
+    tecs_node.setDouble("error_pot", error_pot);
+    tecs_node.setDouble("error_kin", error_kin);
+    
+    // Compute min & max kinetic energy allowed (based on configured
+    // operational speed range)
     double min_kt = tecs_config_node.getDouble("min_kt");
     if ( min_kt < 15 ) { min_kt = 15;}
     double min_mps = min_kt * SG_KT_TO_MPS;
     double min_kinetic = 0.5 * mass_kg * min_mps * min_mps;
-    double min_error = min_kinetic - energy_kin;
 
     double max_kt = tecs_config_node.getDouble("max_kt");
     if ( max_kt < 15 ) { max_kt = 2 * min_kt; }
     double max_mps = max_kt * SG_KT_TO_MPS;
     double max_kinetic = 0.5 * mass_kg * max_mps * max_mps;
+
+    // Set min & max kinetic energy errors allowed (prevent us from
+    // exceeding allowed kinetic energy range)
+    double min_error = min_kinetic - energy_kin;
     double max_error = max_kinetic - energy_kin;
 
-    // Guard against overspeed situations (presumably we've hit our
-    // max pitch up limit and have enough power to accelerate past our
-    // top speed.)  Is there a way to limit error_total using energy
-    // error metrics?  Or do we just live with the potential of
-    // overspeed climbs with over powered aircraft?
-    if ( max_error < 0 ) {
-        // we are too fast
-        // printf("overspeed: %.1f %.1f %.1f\n", error_total, error_kin, error_pot);
-    }     
-        
-    tecs_node.setDouble("error_total", error_total);
-    tecs_node.setDouble("error_pot", error_pot);
-    tecs_node.setDouble("error_kin", error_kin);
+    // if min_error > 0: we are underspeed
+    // if max_error < 0: we are overspeed
     
+    // total energy error and (weighted) energy balance
+    double error_total = error_pot + error_kin;
     double error_diff =  (2.0 - wb) * error_kin - wb * error_pot;
-    // printf("%.1f  %.1f  %.1f\n", min_error, error_diff, max_error);
-
-    // enforce speed limits (in energy error space)
+    
+    // Note: we can clamp the total error and error balance (in energy
+    // error space) to establish speed limits.  This prevents a large
+    // altitude error for over or underspeeding the aircraft.  Also,
+    // prevents over speeding in a climb if max pitch angle is
+    // saturated.
+    
+    // clamp error_diff to kinetic error range
     if ( error_diff < min_error ) { error_diff = min_error; }
     if ( error_diff > max_error ) { error_diff = max_error; }
-    
+
+    // clamp max total error to avoid overspeed in a climb if max
+    // pitch angle is saturated.
+    if ( error_total > max_error ) { error_total = max_error; }
+
+    // publish the final values
+    tecs_node.setDouble("error_total", error_total);
     tecs_node.setDouble("error_diff", error_diff);
 }
