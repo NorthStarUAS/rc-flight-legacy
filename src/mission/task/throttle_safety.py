6d1ff4a1ac14b0698dc14cc1578813f6fb08c696
Curtis L. Olson <curtolson@flightgear.org>
2018-07-21 18:17:08 -0500

    Add a 'safe on touchdown' mode to complement the 'on ground' mode. 'on ground' will always safe the throttle whenever the aircraft is not flying. 'on touchdown' will allow the throttle to run on launch, but will safe it on touchdown.
    Note that this safety feature only applies with autopilot flight mode is
    enabled.  In manual mode the autopilot has no control over the throttle.
    This feature is largely intended to avoid unintentional throttle run ups
    on the ground.  Please configure and use with caution!

--- src/mission/task/throttle_safety.py
+++ src/mission/task/throttle_safety.py
@@ -12,31 +12,40 @@ class ThrottleSafety(Task):
         
         # initial defaults are locked down (this is kind of a big deal!)
         self.master_safety = True
-        self.safety_on_ground = True
+        self.safety_mode = 'on_ground'
+        self.airborne_latch = False
         self.act_node.setBool("throttle_safety", True)
         
         self.name = config_node.getString("name")
         self.nickname = config_node.getString("nickname")
-        self.safety_on_ground = config_node.getBool("safety_on_ground")
+        if config_node.hasChild("safety_mode"):
+            self.safety_mode = config_node.getString('safety_mode')
 
     def activate(self):
         self.active = True
-        comms.events.log("safety", "throttle_safety: " + str(self.master_safety))
+        comms.events.log("safety", "throttle_safety: " + str(self.safety_mode) + " " + str(self.master_safety))
     
     def update(self, dt):
         if not self.active:
             return False
 
+        is_airborne = self.task_node.getBool("is_airborne")
+        if not self.airborne_latch and is_airborne:
+            self.airborne_latch = True
+            
         if self.master_safety:
             # safety is on, check if we should remove it (so throttle can run.)
             if not self.gps_node.getBool("settle"):
                 # do not enable autopilot throttle control if gps
                 # hasn't reported a fix and is settled.
                 pass
-            elif self.safety_on_ground and \
-                 not self.task_node.getBool("is_airborne"):
-                # safety_on_ground==true means never run throttle
-                # unless we are airborne.
+            elif self.safety_mode == 'on_ground' and not is_airborne:
+                # safety 'on_ground' means never run throttle unless we
+                # are airborne.
+                pass
+            elif self.safety_mode == 'on_touchdown' and not is_airborne and self.airborne_latch:
+                # safety 'on_touchdown' means safety starts on, but is
+                # switched off after touchdown (assuming some flying happens)
                 pass
             else:
                 self.master_safety = False
@@ -44,8 +53,13 @@ class ThrottleSafety(Task):
                 comms.events.log("safety", "throttle enabled (safety turned off!)")
         else:
             # safety is off, check if we should turn it on
-            if self.safety_on_ground and \
-               not self.task_node.getBool("is_airborne"):
+            make_safe = False
+            print(self.airborne_latch, self.safety_mode, is_airborne)
+            if self.safety_mode == 'on_ground' and not is_airborne:
+                make_safe = True
+            elif self.safety_mode == 'on_touchdown' and self.airborne_latch and not is_airborne:
+                make_safe = True
+            if make_safe:
                 self.master_safety = True
                 self.act_node.setBool("throttle_safety", self.master_safety)
                 comms.events.log("safety", "Throttle disabled (safety turned on!)")
