b27783456a0021241d7b3cf014f59bee61216bd8
Curtis L. Olson <curtolson@flightgear.org>
2018-09-14 14:54:15 -0500

    Working on setting up a better infrastructure for python wrapped C code.  The goal is to optimize some core functionality that the python scripts use to improve the overall efficiency of the flight code app.

---
+++ python/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# a quick first hack/test
+
+from setuptools import setup, Extension
+
+setup(name='auracore',
+      version='1.0',
+      description='test',
+      author='Curtis L. Olson',
+      author_email='curtolson@flightgear.org',
+      url='https://github.com/AuraUAS',
+      ext_modules=[
+            Extension('auracore.wgs84',
+                      define_macros=[('HAVE_PYBIND11', '1')],
+                      sources=['util/wgs84.cxx'],
+                      )
+          ]
+      )
