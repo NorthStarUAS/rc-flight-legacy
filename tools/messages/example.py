7694b5a8d6df9ce83d109af045089fbd8e0c22f5
Curtis L. Olson <curtolson@flightgear.org>
2019-06-23 08:46:08 -0500

    Parallel the C++ example program.

--- tools/messages/example.py
+++ tools/messages/example.py
@@ -14,19 +14,13 @@ print("received:", st_recv.a)
 # gps v4 test
 gps = messages.gps_v4()
 gps.index = 0
-gps.time_sec = 15.12345
 gps.latitude_deg = 43.241
 gps.longitude_deg = -93.520
 gps.altitude_m = 278.5
 gps.vn_ms = 1.5
 gps.ve_ms = -2.7
 gps.vd_ms = -0.02
-gps.unixtime_sec = 1508247829.23
 gps.satellites = 9
-gps.horiz_accuracy_m = 1.7
-gps.vert_accuracy_m = 2.8
-gps.pdop = 4.2
-gps.fix_type = 3
 msg = gps.pack()
 print("msg len:", len(msg))
 
