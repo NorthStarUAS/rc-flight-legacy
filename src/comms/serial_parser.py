68e471c8225aaaeb907be1cd7d2260a17e6f2727
Curtis L. Olson <curtolson@flightgear.org>
2019-06-28 10:38:37 -0500

    Clean up serial parser a bit, and move it towards matching serial_link.cxx/hxx

--- src/comms/serial_parser.py
+++ src/comms/serial_parser.py
@@ -8,29 +8,32 @@ import time
 START_OF_MSG0 = 147
 START_OF_MSG1 = 224
 
-# FIXME: I like this feature which can catch ascii messages injected
-# in the output, although with newest code and newest hardware with a
-# dedicated uart for messages, this is actually deprecated.
-ascii_message = ''
-def glean_ascii_msgs(c, display=False):
-    global ascii_message
-    if str(c) in string.printable:
-        ascii_message += str(c)
-    elif len(ascii_message) > 4:
-        if display: print(ascii_message)
-        ascii_message = ''
-
 class serial_parser():
     def __init__(self):
         self.state = 0
         self.pkt_id = 0
         self.pkt_len = 0
         self.counter = 0
-        self.cksum_A = 0
-        self.cksum_B = 0
         self.cksum_lo = 0
         self.cksum_hi = 0
         self.payload = bytearray()
+
+    # simple 2-byte checksum
+    def checksum(self, id, buf, size):
+        c0 = 0
+        c1 = 0
+        c0 = (c0 + id) & 0xff
+        c1 = (c1 + c0) & 0xff
+        #print("c0 =", c0, "c1 =", c1)
+        c0 = (c0 + size) & 0xff
+        c1 = (c1 + c0) & 0xff
+        #print("c0 =", c0, "c1 =", c1)
+        for i in range(0, size):
+            c0 = (c0 + buf[i]) & 0xff
+            c1 = (c1 + c0) & 0xff
+            #print("c0 =", c0, "c1 =", c1, i, '[', buf[i], ']')
+        #print("c0 =", c0, "c1 =", c1)
+        return (c0, c1)
 
     def read(self, ser):
         start_time = time.time()    # sec
@@ -39,12 +42,9 @@ class serial_parser():
         if self.state == 0:
             self.counter = 0
             self.payload = bytearray()
-            self.cksum_A = 0
-            self.cksum_B = 0
             input = ser.read(1)
             while len(input) and input[0] != START_OF_MSG0:
                 # print(" state0 val:", input[0])
-                glean_ascii_msgs(input[0], display=False)
                 input = ser.read(1)
                 cur_time = time.time()
                 if cur_time > start_time + 0.1:
@@ -68,8 +68,6 @@ class serial_parser():
             input = ser.read(1)
             if len(input):
                 self.pkt_id = input[0]
-                self.cksum_A = (self.cksum_A + input[0]) & 0xff
-                self.cksum_B = (self.cksum_B + self.cksum_A) & 0xff
                 #print(" pkt_id:", self.pkt_id)
                 self.state += 1
         if self.state == 3:
@@ -78,8 +76,6 @@ class serial_parser():
                 self.pkt_len = input[0]
                 # print " pkt_len:", self.pkt_len
                 # print " payload =",
-                self.cksum_A = (self.cksum_A + input[0]) & 0xff
-                self.cksum_B = (self.cksum_B + self.cksum_A) & 0xff
                 self.state += 1
         if self.state == 4:
             input = ser.read(1)
@@ -87,8 +83,6 @@ class serial_parser():
                 self.counter += 1
                 self.payload.append(input[0])
                 # print "%02X" % input[0],
-                self.cksum_A = (self.cksum_A + input[0]) & 0xff
-                self.cksum_B = (self.cksum_B + self.cksum_A) & 0xff
                 if self.counter >= self.pkt_len:
                     self.state += 1
                     # print ""
@@ -104,14 +98,30 @@ class serial_parser():
             input = ser.read(1)
             if len(input):
                 self.cksum_hi = input[0]
+                (cksum0, cksum1) = self.checksum( self.pkt_id, self.payload, self.pkt_len );
                 # print " cksum_hi:", self.cksum_hi
-                if self.cksum_A == self.cksum_lo and self.cksum_B == self.cksum_hi and self.pkt_len > 0:
+                if cksum0 == self.cksum_lo and cksum1 == self.cksum_hi and self.pkt_len > 0:
                     #print "checksum passes:", self.pkt_id, "len:", self.pkt_len
                     self.state = 0
                     return self.pkt_id
                 else:
-                    # print "pkt id=%d checksum failed %d %d (computed) != %d %d (message)" % (self.pkt_id, self.cksum_A, self.cksum_B, self.cksum_lo, self.cksum_hi)
+                    # print "pkt id=%d checksum failed %d %d (computed) != %d %d (message)" % (self.pkt_id, cksum0, cksum1, self.cksum_lo, self.cksum_hi)
                     print("pkt id=%d len=%d checksum failed" % (self.pkt_id, self.pkt_len))
                     self.state = 0
 
         return -1
+    
+    # wrap payload in header bytes, id, length, payload, and compute checksums
+    def wrap_packet( packet_id, payload ):
+        size = len(payload)
+        buf = bytearray()
+        buf.append(START_OF_MSG0)   # start of message sync bytes
+        buf.append(START_OF_MSG1)   # start of message sync bytes
+        buf.append(packet_id)       # packet id (1 byte)
+        buf.append(size)            # packet size (1 byte)
+        buf.extend(payload)         # copy payload
+        (cksum0, cksum1) = compute_cksum( packet_id, payload, size)
+        buf.append(cksum0)          # check sum byte 1
+        buf.append(cksum1)          # check sum byte 2
+        return buf
+    
