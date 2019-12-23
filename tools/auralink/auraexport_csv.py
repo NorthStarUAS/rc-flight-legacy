5994d0f6deceda8861f119b793403a71a1713c3d
Curtis L. Olson <curtolson@flightgear.org>
2019-12-23 10:23:20 -0600

    Some messaging updates that were left uncommitted.

--- tools/auralink/auraexport_csv.py
+++ tools/auralink/auraexport_csv.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 from props import root, getNode
 
 sys.path.append("../../src")
+from comms import aura_messages
 from comms.packet_id import *
 import comms.packer
 
@@ -23,25 +24,23 @@ import auraparser
 m2nm   = 0.0005399568034557235 # meters to nautical miles
 
 def logical_category(id):
-    if id == GPS_PACKET_V2 or id == GPS_PACKET_V3 or id == GPS_PACKET_V4:
+    if id == aura_messages.gps_v2_id or id == aura_messages.gps_v3_id or id == aura_messages.gps_v4_id:
         return 'gps'
-    elif id == IMU_PACKET_V3 or id == IMU_PACKET_V4:
+    elif id == aura_messages.imu_v3_id or id == aura_messages.imu_v4_id:
         return 'imu'
-    elif id == AIRDATA_PACKET_V5 or id == AIRDATA_PACKET_V6 or id == AIRDATA_PACKET_V7:
+    elif id == aura_messages.airdata_v5_id or id == aura_messages.airdata_v6_id or id == aura_messages.airdata_v7_id:
         return 'air'
-    elif id == FILTER_PACKET_V2 or id == FILTER_PACKET_V3 \
-         or id == FILTER_PACKET_V4:
+    elif id == aura_messages.filter_v2_id or id == aura_messages.filter_v3_id or id == aura_messages.filter_v4_id:
         return 'filter'
-    elif id == ACTUATOR_PACKET_V2 or id == ACTUATOR_PACKET_V3:
+    elif id == aura_messages.actuator_v2_id or id == aura_messages.actuator_v3_id:
         return 'act'
-    elif id == id == PILOT_INPUT_PACKET_V2 or id == PILOT_INPUT_PACKET_V3:
+    elif id == id == aura_messages.pilot_v2_id or id == aura_messages.pilot_v3_id:
         return 'pilot'
-    elif id == AP_STATUS_PACKET_V4 or id == AP_STATUS_PACKET_V5 \
-         or id == AP_STATUS_PACKET_V6 or id == AP_STATUS_PACKET_V7:
+    elif id == aura_messages.ap_status_v4_id or id == aura_messages.ap_status_v5_id or id == aura_messages.ap_status_v6_id or id == aura_messages.ap_status_v7_id:
         return 'ap'
-    elif id == SYSTEM_HEALTH_PACKET_V4 or id == SYSTEM_HEALTH_PACKET_V5:
+    elif id == aura_messages.system_health_v4_id or id == aura_messages.system_health_v5_id:
         return 'health'
-    elif id == PAYLOAD_PACKET_V2 or id == PAYLOAD_PACKET_V3:
+    elif id == aura_messages.payload_v2_id or id == aura_messages.payload_v3_id:
         return 'payload'
     elif id == EVENT_PACKET_V1:
         return 'event'
