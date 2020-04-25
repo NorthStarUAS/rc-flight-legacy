bf2df6eed2eb90fc9710ff1a8ef46453bba49d0d
Curtis L. Olson <curtolson@flightgear.org>
2020-04-25 09:02:16 -0500

    compute and display the final rotation matrix.

--- src/mission/task/calib_accels.py
+++ src/mission/task/calib_accels.py
@@ -1,5 +1,5 @@
 import math
-import numpy
+import numpy as np
 
 from props import getNode
 
@@ -7,14 +7,16 @@ import comms.events
 from mission.task.task import Task
 from mission.task.lowpass import LowPass
 
+g = 9.81                        # gravity
+
 # state key:
-#   0 = not started
-#   1 = right side up
-#   2 = up side down
-#   3 = nose down
-#   4 = nose up
-#   5 = right wing up
-#   6 = right wing down
+#   0 = right side up
+#   1 = up side down
+#   2 = nose down
+#   3 = nose up
+#   4 = right wing up
+#   5 = right wing down
+#   6 = sanity check
 #   7 = complete ok
 #   8 = complete failed
 
@@ -44,23 +46,23 @@ def affine_matrix_from_points(v0, v1, shear=True, scale=True, usesvd=True, usesp
     array([[   0.14549,    0.00062,  675.50008],
            [   0.00048,    0.14094,   53.24971],
            [   0.     ,    0.     ,    1.     ]])
-    >>> T = translation_matrix(numpy.random.random(3)-0.5)
-    >>> R = random_rotation_matrix(numpy.random.random(3))
+    >>> T = translation_matrix(np.random.random(3)-0.5)
+    >>> R = random_rotation_matrix(np.random.random(3))
     >>> S = scale_matrix(random.random())
     >>> M = concatenate_matrices(T, R, S)
-    >>> v0 = (numpy.random.rand(4, 100) - 0.5) * 20
+    >>> v0 = (np.random.rand(4, 100) - 0.5) * 20
     >>> v0[3] = 1
-    >>> v1 = numpy.dot(M, v0)
-    >>> v0[:3] += numpy.random.normal(0, 1e-8, 300).reshape(3, -1)
+    >>> v1 = np.dot(M, v0)
+    >>> v0[:3] += np.random.normal(0, 1e-8, 300).reshape(3, -1)
     >>> M = affine_matrix_from_points(v0[:3], v1[:3])
-    >>> numpy.allclose(v1, numpy.dot(M, v0))
+    >>> np.allclose(v1, np.dot(M, v0))
     True
 
     More examples in superimposition_matrix()
 
     """
-    v0 = numpy.array(v0, dtype=numpy.float64, copy=True)
-    v1 = numpy.array(v1, dtype=numpy.float64, copy=True)
+    v0 = np.array(v0, dtype=np.float64, copy=True)
+    v1 = np.array(v1, dtype=np.float64, copy=True)
 
     # print( "v0.shape = %s" % str(v0.shape))
     # print( "v1.shape = %s" % str(v1.shape))
@@ -70,53 +72,53 @@ def affine_matrix_from_points(v0, v1, shear=True, scale=True, usesvd=True, usesp
         raise ValueError("input arrays are of wrong shape or type")
 
     # move centroids to origin
-    t0 = -numpy.mean(v0, axis=1)
-    M0 = numpy.identity(ndims+1)
+    t0 = -np.mean(v0, axis=1)
+    M0 = np.identity(ndims+1)
     M0[:ndims, ndims] = t0
     v0 += t0.reshape(ndims, 1)
-    t1 = -numpy.mean(v1, axis=1)
-    M1 = numpy.identity(ndims+1)
+    t1 = -np.mean(v1, axis=1)
+    M1 = np.identity(ndims+1)
     M1[:ndims, ndims] = t1
     v1 += t1.reshape(ndims, 1)
 
     if shear:
         # Affine transformation
-        A = numpy.concatenate((v0, v1), axis=0)
+        A = np.concatenate((v0, v1), axis=0)
         if usesparse:
             u, s, vh = scipy.sparse.linalg.svds(A.T, k=3)
         else:
-            u, s, vh = numpy.linalg.svd(A.T)
+            u, s, vh = np.linalg.svd(A.T)
         vh = vh[:ndims].T
         B = vh[:ndims]
         C = vh[ndims:2*ndims]
-        t = numpy.dot(C, numpy.linalg.pinv(B))
-        t = numpy.concatenate((t, numpy.zeros((ndims, 1))), axis=1)
-        M = numpy.vstack((t, ((0.0,)*ndims) + (1.0,)))
+        t = np.dot(C, np.linalg.pinv(B))
+        t = np.concatenate((t, np.zeros((ndims, 1))), axis=1)
+        M = np.vstack((t, ((0.0,)*ndims) + (1.0,)))
     elif usesvd or ndims != 3:
         # Rigid transformation via SVD of covariance matrix
-        u, s, vh = numpy.linalg.svd(numpy.dot(v1, v0.T))
+        u, s, vh = np.linalg.svd(np.dot(v1, v0.T))
         # rotation matrix from SVD orthonormal bases
-        R = numpy.dot(u, vh)
-        if numpy.linalg.det(R) < 0.0:
+        R = np.dot(u, vh)
+        if np.linalg.det(R) < 0.0:
             # R does not constitute right handed system
-            R -= numpy.outer(u[:, ndims-1], vh[ndims-1, :]*2.0)
+            R -= np.outer(u[:, ndims-1], vh[ndims-1, :]*2.0)
             s[-1] *= -1.0
         # homogeneous transformation matrix
-        M = numpy.identity(ndims+1)
+        M = np.identity(ndims+1)
         M[:ndims, :ndims] = R
     else:
         # Rigid transformation matrix via quaternion
         # compute symmetric matrix N
-        xx, yy, zz = numpy.sum(v0 * v1, axis=1)
-        xy, yz, zx = numpy.sum(v0 * numpy.roll(v1, -1, axis=0), axis=1)
-        xz, yx, zy = numpy.sum(v0 * numpy.roll(v1, -2, axis=0), axis=1)
+        xx, yy, zz = np.sum(v0 * v1, axis=1)
+        xy, yz, zx = np.sum(v0 * np.roll(v1, -1, axis=0), axis=1)
+        xz, yx, zy = np.sum(v0 * np.roll(v1, -2, axis=0), axis=1)
         N = [[xx+yy+zz, 0.0,      0.0,      0.0],
              [yz-zy,    xx-yy-zz, 0.0,      0.0],
              [zx-xz,    xy+yx,    yy-xx-zz, 0.0],
              [xy-yx,    zx+xz,    yz+zy,    zz-xx-yy]]
         # quaternion: eigenvector corresponding to most positive eigenvalue
-        w, V = numpy.linalg.eigh(N)
-        q = V[:, numpy.argmax(w)]
+        w, V = np.linalg.eigh(N)
+        q = V[:, np.argmax(w)]
         q /= vector_norm(q)  # unit quaternion
         # homogeneous transformation matrix
         M = quaternion_matrix(q)
@@ -125,10 +127,10 @@ def affine_matrix_from_points(v0, v1, shear=True, scale=True, usesvd=True, usesp
         # Affine transformation; scale is ratio of RMS deviations from centroid
         v0 *= v0
         v1 *= v1
-        M[:ndims, :ndims] *= math.sqrt(numpy.sum(v1) / numpy.sum(v0))
+        M[:ndims, :ndims] *= math.sqrt(np.sum(v1) / np.sum(v0))
 
     # move centroids back
-    M = numpy.dot(numpy.linalg.inv(M1), numpy.dot(M, M0))
+    M = np.dot(np.linalg.inv(M1), np.dot(M, M0))
     M /= M[ndims, ndims]
     return M
 
@@ -136,6 +138,7 @@ class CalibrateAccels(Task):
     def __init__(self, config_node):
         Task.__init__(self)
         self.imu_node = getNode("/sensors/imu", True)
+        self.config_imu_node = getNode("/config/drivers/Aura4/imu")
         self.state = 0
         self.ax_slow = LowPass(time_factor=2.0) 
         self.ax_fast = LowPass(time_factor=0.2) 
@@ -144,12 +147,12 @@ class CalibrateAccels(Task):
         self.az_slow = LowPass(time_factor=2.0) 
         self.az_fast = LowPass(time_factor=0.2)
         self.armed = False
-        self.ref = [ [  0.0,   0.0, -9.81 ],
-                     [  0.0,   0.0,  9.81 ],
-                     [ -9.81,  0.0,   0.0 ],
-                     [  9.81,  0.0,   0.0 ],
-                     [  0.0,  -9.81,  0.0 ],
-                     [  0.0,   9.81,  0.0 ] ]
+        self.ref = [ [  0,  0, -g ],
+                     [  0,  0,  g ],
+                     [ -g,  0,  0 ],
+                     [  g,  0,  0 ],
+                     [  0, -g,  0 ],
+                     [  0,  g,  0 ] ]
         self.meas = list(self.ref) # copy
         self.checked = {}
         
@@ -264,13 +267,56 @@ class CalibrateAccels(Task):
                 self.checked[up_axis] = True
                 self.state += 1
                 self.armed = False
+        elif self.state == 6:
+            # did we measure 6 unique axes?
+            if len(self.state) != 6:
+                self.state += 2
+            else:
+                # compute affine rotation fit
+                v0 = np.array(self.meas, dtype=np.float64, copy=True).T
+                v1 = np.array(self.ref, dtype=np.float64, copy=True).T
+                M = affine_matrix_from_points(v0, v1, shear=False, scale=False)
+                R = M[:3,:3]
+                print(R @ R.T)
+                # R should be orthogonal/normalized here
+                # check if any row column doesn't have an element close to 1
+                if abs(np.max(R[0])) < 0.9:
+                    self.state += 2
+                elif abs(np.max(R[1])) < 0.9:
+                    self.state += 2
+                elif abs(np.max(R[2])) < 0.9:
+                    self.state += 2
+                elif abs(np.max(R[:,0])) < 0.9:
+                    self.state += 2
+                elif abs(np.max(R[:,1])) < 0.9:
+                    self.state += 2
+                elif abs(np.max(R[:,2])) < 0.9:
+                    self.state += 2
+                else:
+                    # nothing bad detected, save results and goto success state
+                    self.R = R
+                    self.T = M[:,:4][:3] # 1st 3 elements of the 4th column
+                    self.state += 1
+        elif self.state == 7:
+            # calibration complete, success!
+            # consider current orientation matrix if it exists
+            if self.config_imu_node and self.config_imu_node.hasChild("orientation"):
+                current = []
+                for i in range(9):
+                    current.append(self.config_imu_node.getFloatEnum("orientation", i))
+                    current = np.array(current).reshape(3,3)
+            else:
+                current = np.eye(3)
+            print("current:")
+            print(current)
+            print("R:")
+            print(R)
+            print("Final:")
+            print(current @ R)
+        elif self.state == 8:
+            # calibration complete, but failed. :-(
+            pass            
 
-        v0 = numpy.array(self.meas, dtype=numpy.float64, copy=True).T
-        v1 = numpy.array(self.ref, dtype=numpy.float64, copy=True).T
-        M = affine_matrix_from_points(v0, v1, shear=False, scale=False)
-        R = M[:3,:3]
-        print(R * R.T)
-                                  
     def is_complete(self):
         return False
 
