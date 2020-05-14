7593aa15d5df0fffe3967bbb0c73fd0acb18496f
Curtis L. Olson <curtolson@flightgear.org>
2020-05-14 16:18:37 -0500

    Updated to run as aura user and out of home directory.

--- scripts/systemd/bb-aura.sh
+++ scripts/systemd/bb-aura.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-PREFIX=/usr/local
-SOURCE=/home/aura/Source
-echo "starting aura autopilot with prefex=$PREFIX"
+RUNAS=aura
+HOME=`eval echo ~$RUNAS`
+SOURCE=$HOME/Source
+RUNLOG=`mktemp -u -p $HOME -t auralog.XXXX`
 
 # nice range is -20 to 20 with -20 being the highest priority.  -10 should be
 # a good balanced number.  -20 could be a little selfish, but that might be
 # exactly what we want.
 NICE=-20
 
-nice -n $NICE $PREFIX/bin/aura --python_path $SOURCE/aura-core/src --config $SOURCE/aura-config/config > /var/log/aura.log
+# wait for devices to exist (commented out by default)
+#
+#while [ ! -e /dev/ttyUSB0 ]; do
+#	sleep 1
+#done
+#while [ ! -e /dev/ttyACM0 ]; do
+#	sleep 1
+#done
+#sleep 5
+
+nice -n $NICE sudo -i -u $RUNAS -g dialout bash << EOF
+
+echo "Running aura autopilot as $RUNAS"
+echo "Source: $SOURCE"
+echo "Runlog: $RUNLOG"
+
+$SOURCE/aura-core/build/src/aura --python_path $SOURCE/aura-core/src --config $SOURCE/aura-config/config > $RUNLOG
+
+EOF
