957544021fe54dae1abd99eaf4cb6cfabd10edd7
Curtis L. Olson <curtolson@flightgear.org>
2021-09-12 08:58:17 -0500

    Drop all the serial port handling, now telemetry and gcs command relays are handled through the fmu.

--- src/comms/remote_link.py
+++ src/comms/remote_link.py
@@ -1,18 +1,14 @@
 import random
 import re
-import serial
 import time
 
 from PropertyTree import PropertyNode
 
 from comms import rc_messages
 import comms.events
-from comms.packer import packer
-import comms.serial_parser
 
 import survey.survey
 
-status_node = PropertyNode("/status")
 route_node = PropertyNode("/task/route")
 task_node = PropertyNode("/task")
 home_node = PropertyNode("/task/home")
@@ -20,194 +16,14 @@ active_node = PropertyNode("/task/route/active")
 targets_node = PropertyNode("/autopilot/targets")
 comms_node = PropertyNode("/comms")
 
-remote_link_config = PropertyNode("/config/remote_link")
-remote_link_node = PropertyNode("/comms/remote_link")
-
-remote_link_on = False    # link to remote operator station
-ser = None
-parser = None
-serial_buf = bytearray()
-max_serial_buffer = 256
-link_open = False
-
 # set up the remote link
 def init():
-    global ser
-    global parser
-    global link_open
+    pass
     
-    global act_skip
-    global act_count
-    global airdata_skip
-    global airdata_count
-    global ap_skip
-    global ap_count
-    global filter_skip
-    global filter_count
-    global gps_skip
-    global gps_count
-    global health_skip
-    global health_count
-    global imu_skip
-    global imu_count
-    global pilot_skip
-    global pilot_count
-    act_skip = remote_link_config.getInt("actuator_skip")
-    act_count = random.randint(0, act_skip)
-    airdata_skip = remote_link_config.getInt("airdata_skip")
-    airdata_count = random.randint(0, airdata_skip)
-    ap_skip = remote_link_config.getInt("autopilot_skip")
-    ap_count = random.randint(0, ap_skip)
-    filter_skip = remote_link_config.getInt("filter_skip")
-    filter_count = random.randint(0, filter_skip)
-    gps_skip = remote_link_config.getInt("gps_skip")
-    gps_count = random.randint(0, gps_skip)
-    health_skip = remote_link_config.getInt("health_skip")
-    health_count = random.randint(0, health_skip)
-    imu_skip = remote_link_config.getInt("imu_skip")
-    imu_count = random.randint(0, imu_skip)
-    pilot_skip = remote_link_config.getInt("pilot_skip")
-    pilot_count = random.randint(0, pilot_skip)
-
-    if remote_link_config.hasChild("device"):
-        device = remote_link_config.getString("device")
-    else:
-        return;
-
-    while not link_open:
-        try:
-            ser = serial.Serial(port=device, baudrate=115200, timeout=0, writeTimeout=0)
-            parser = comms.serial_parser.serial_parser()
-            link_open = True
-        except Exception as e:
-            print("Opening remote link failed:", device)
-            print(e)
-            print("sleeping 1 ...")
-            time.sleep(1)
-
-    if comms_node.getBool("display_on"):
-        print("remote link:", device)
-
-    remote_link_node.setInt("sequence_num", 0)
-    if not remote_link_config.getInt("write_bytes_per_frame"):
-        remote_link_config.setInt("write_bytes_per_frame", 12)
-
-# write as many bytes out of the serial_buf to the uart as the
-# driver will accept.
-def flush_serial():
-    global serial_buf
-    if not link_open:
-        # device not open
-        return
-
-    # write a constrained chunk of available bytes per frame to avoid
-    # overflowing the system serial port buffer
-    bytes_per_frame = remote_link_config.getInt("write_bytes_per_frame")
-    write_len = len(serial_buf)
-    if write_len > bytes_per_frame:
-        write_len = bytes_per_frame
-    if write_len:
-        bytes_written = ser.write( serial_buf[:write_len] )
-        # print("avail = %d  written = %d" % (len(serial_buf), bytes_written))
-        if bytes_written < 0:
-            # perror("serial write")
-            pass
-        elif bytes_written == 0:
-            # nothing was written
-            pass
-        else:
-            # something was written
-            serial_buf = serial_buf[bytes_written:]
-
-# append the request data to a fifo buffer if space available.  A
-# separate function will flush the data in even chunks to avoid
-# saturating the telemetry link.
-def send_message( pkt_id, payload ):
-    global serial_buf
-    if ser == None:
-        # remote serial link not available
-        return False
-
-    msg = comms.serial_parser.wrap_packet(pkt_id, payload)
-    if len(serial_buf) + len(msg) <= max_serial_buffer:
-        serial_buf.extend(msg)
-        return True
-    else:
-        if comms_node.getBool("display_on"):
-            print("remote link serial buffer overflow, size:", len(serial_buf), "add:", len(msg), "limit:", max_serial_buffer)
-        return False
-
-# build messages and send them as needed
-def process_messages():
-    global act_count
-    global airdata_count
-    global ap_count
-    global filter_count
-    global gps_count
-    global health_count
-    global imu_count
-    global pilot_count
-    act_count -= 1
-    airdata_count -= 1
-    ap_count -= 1
-    filter_count -= 1
-    gps_count -= 1
-    health_count -= 1
-    imu_count -= 1
-    pilot_count -= 1
-    if act_count < 0:
-        act_count = act_skip
-        buf = packer.pack_act_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.act.id, buf)
-    if airdata_count < 0:
-        airdata_count = airdata_skip
-        buf = packer.pack_airdata_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.airdata.id, buf)
-    if ap_count < 0:
-        ap_count = ap_skip
-        buf = packer.pack_ap_status_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.ap.id, buf)
-            # here is where we do the delicate counter increment dance
-            # (vs. packer.py)
-            route_size = active_node.getInt("route_size")
-            counter = remote_link_node.getInt("wp_counter") + 1
-            if counter >= route_size + 2:
-                counter = 0
-            remote_link_node.setInt("wp_counter", counter) 
-    if filter_count < 0:
-        filter_count = filter_skip
-        buf = packer.pack_filter_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.filter.id, buf)
-    if gps_count < 0:
-        gps_count = gps_skip
-        buf = packer.pack_gps_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.gps.id, buf)
-    if health_count < 0:
-        health_count = health_skip
-        buf = packer.pack_system_health_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.health.id, buf)
-    if imu_count < 0:
-        imu_count = imu_skip
-        buf = packer.pack_imu_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.imu.id, buf)
-    if pilot_count < 0:
-        pilot_count = pilot_skip
-        buf = packer.pack_pilot_bin(use_cached=True)
-        if not buf is None and len(buf):
-            send_message(packer.pilot.id, buf)
-        
 def update():
-    process_messages()
-    flush_serial();
+    process_commands()
 
-def process_commands_new():
+def process_commands():
     len = comms_node.getLen("commands")
     for i in range(len):
         command = comms_node.getString("commands", i)
@@ -364,46 +180,6 @@ def execute_command( command ):
             wgs84_node.setDouble( "latitude_deg", lat )
             ground = pos_node.getDouble("altitude_ground_m")
             wgs84_node.setDouble( "altitude_m", ground )
-
-def read_link_command():
-    global ser
-    
-    if parser == None:
-        # remote link open failed
-        return -1, ""
-    
-    pkt_id = parser.read(ser)
-    if pkt_id == rc_messages.command_v1_id:
-        cmd = rc_messages.command_v1(parser.payload)
-        return cmd.sequence_num, cmd.message
-    else:
-        return -1, ""
-
-
-# read, parse, and execute incomming commands, return True if a valid
-# command received, False otherwise.
-last_sequence_num = -1
-def command():
-    global last_sequence_num
-    
-    sequence_num, command = read_link_command()
-    if sequence_num < 0:
-        return False
-    
-    # ignore repeated commands (including roll over logic)
-    if sequence_num != last_sequence_num:
-	# execute command
-        comms.events.log( "remote command",
-                          "executed: (%d) %s" % (sequence_num, command) )
-        execute_command( command )
-
-        # register that we"ve received this message correctly
-        remote_link_node.setInt( "sequence_num", sequence_num )
-        last_sequence_num = sequence_num
-        timestamp = status_node.getDouble("frame_time")
-        remote_link_node.setDouble( "last_message_sec", timestamp )
-
-    return True
 
 def decode_fcs_update(command):
     tokens = command.split(",")
